750f868637ec4f7cf40f1ec47185b7eee933d371
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-01-12 23:01:37 +0330

    Update base url

--- config/axios.ts
+++ config/axios.ts
@@ -1,7 +1,7 @@
 import axiosModule from "axios";
 
 const axios = axiosModule.create({
-    baseURL : "https://filmseries.iran.liara.run"
+    baseURL: "https://artgram.iran.liara.run",
 });
 
 export default axios;
