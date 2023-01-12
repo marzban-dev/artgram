5b2b4ba4a8b50f8eafcaeded87be86562ead3cfe
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-01-12 23:03:23 +0330

    Make useImageColors hook flexible

--- hooks/useImageColors.ts
+++ hooks/useImageColors.ts
@@ -1,5 +1,5 @@
+import { FastAverageColor } from "fast-average-color";
 import { useEffect, useState } from "react";
-import { FastAverageColor, FastAverageColorResult } from "fast-average-color";
 // @ts-ignore
 import Color from "color";
 
@@ -8,7 +8,17 @@ interface IColorsResult {
     lighter: string;
 }
 
-const useImageColors = (query: string) => {
+type TUseImageColorsParams = (
+    query: string,
+    options?: {
+        darkPrimary?: number;
+        lightPrimary?: number;
+        darkLighter?: number;
+        lightLighter?: number;
+    }
+) => IColorsResult | null;
+
+const useImageColors: TUseImageColorsParams = (query, options) => {
     const [imageColors, setImageColors] = useState<IColorsResult | null>(null);
 
     useEffect(() => {
@@ -23,11 +33,19 @@ const useImageColors = (query: string) => {
                 };
 
                 if (color.isDark) {
-                    colorsObject.primary = Color(color.rgb).lighten(0.5).hex();
-                    colorsObject.lighter = Color(color.rgb).lighten(0.6).hex();
+                    colorsObject.primary = Color(color.rgb)
+                        .lighten(options?.darkPrimary ? options?.darkPrimary : 0.5)
+                        .hex();
+                    colorsObject.lighter = Color(color.rgb)
+                        .lighten(options?.darkLighter ? options?.darkLighter : 0.6)
+                        .hex();
                 } else {
-                    colorsObject.primary = Color(color.rgb).darken(0.3).hex();
-                    colorsObject.lighter = Color(color.rgb).darken(0.2).hex();
+                    colorsObject.primary = Color(color.rgb)
+                        .darken(options?.lightPrimary ? options?.lightPrimary : 0.3)
+                        .hex();
+                    colorsObject.lighter = Color(color.rgb)
+                        .darken(options?.lightLighter ? options?.lightLighter : 0.2)
+                        .hex();
                 }
 
                 setImageColors(colorsObject);
