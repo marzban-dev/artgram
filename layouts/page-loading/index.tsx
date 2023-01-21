636f1ffaa107087d5300eaeb7bdc222173451c68
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-01-21 14:50:18 +0330

    Update page-loading layout component

--- layouts/page-loading/index.tsx
+++ layouts/page-loading/index.tsx
@@ -1,16 +1,10 @@
 import Spinner from "components/spinner";
-import { motion } from "framer-motion";
 
 const PageLoading: React.FC = () => {
     return (
-        <motion.div
-            className="w-full h-screen bg-black flex justify-center items-center"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-        >
+        <div className="w-full h-screen bg-black flex justify-center items-center">
             <Spinner size={45} />
-        </motion.div>
+        </div>
     );
 };
 
