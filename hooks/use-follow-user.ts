f534a4033b419ae5f66408a876e3dc5cc7221c73
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-01-21 14:49:16 +0330

    Create use-follow-user hook

---
+++ hooks/use-follow-user.ts
@@ -0,0 +1,28 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { followUser } from "api/user.api";
+
+export const useFollowUser = (id: number | string) => {
+    const queryClient = useQueryClient();
+    const key = ["user-followers", id];
+
+    return useMutation({
+        mutationFn: followUser,
+        onMutate: async () => {
+            await queryClient.cancelQueries();
+            const prevQueryData = queryClient.getQueryData(key);
+
+            queryClient.setQueryData<number>(key, (prevQueryData) => {
+                if (prevQueryData) return prevQueryData - 1;
+                return prevQueryData;
+            });
+
+            return { prevQueryData };
+        },
+        onError: (_err, _data, ctx) => {
+            queryClient.setQueryData(key, ctx!.prevQueryData);
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries(key);
+        },
+    });
+};
