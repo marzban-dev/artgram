ef89f7c347ed2188477d0192caedb8905a970b96
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-01-21 14:55:14 +0330

    Update _app component

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,11 +1,45 @@
+import { Hydrate, QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { AnimatePresence } from "framer-motion";
+import useRouteLoading from "hooks/use-route-loading";
+import PageLoading from "layouts/page-loading";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
-import Head from "next/head";
+import { useRouter } from "next/router";
+import { useRef } from "react";
+import { Toaster, DefaultToastOptions } from "react-hot-toast";
+import { Provider } from "react-redux";
+import store from "store";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
+    const { pathname } = useRouter();
+    const isRouteLoading = useRouteLoading();
+    const queryClient = useRef(new QueryClient());
+
+    const toastOptions: DefaultToastOptions = {
+        position : "bottom-center",
+        style: {
+            borderRadius: "5px",
+            background: "#1c1c1c",
+            color: "#fff",
+            padding: "10px 20px",
+        },
+    };
+
     return (
-        <>
-            <Component {...pageProps} />
-        </>
+        <Provider store={store}>
+            <SessionProvider session={pageProps.session}>
+                <QueryClientProvider client={queryClient.current}>
+                    <Hydrate state={pageProps.dehydratedState}>
+                        <AnimatePresence mode="wait" initial={false}>
+                            {isRouteLoading ? <PageLoading key={1} /> : <Component key={pathname} {...pageProps} />}
+                        </AnimatePresence>
+                        <Toaster toastOptions={toastOptions} position="bottom-center" reverseOrder={false} />
+                    </Hydrate>
+                    <ReactQueryDevtools initialIsOpen={false} />
+                </QueryClientProvider>
+            </SessionProvider>
+        </Provider>
     );
 }
