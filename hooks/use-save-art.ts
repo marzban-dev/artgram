5bbc1c94ab5aee1809614cb4981e4d12e420c833
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-11 22:42:33 +0330

    Add use-save-art hook

---
+++ hooks/use-save-art.ts
@@ -0,0 +1,33 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { saveArt } from "api/user.api";
+
+export const useSaveArtMutation = () => {
+    const queryClient = useQueryClient();
+    const key = ["user-saves"];
+
+    return useMutation({
+        mutationFn: saveArt,
+        onMutate: async (data) => {
+            await queryClient.cancelQueries();
+            const prevQueryData = queryClient.getQueryData(key);
+
+            queryClient.setQueryData<number[]>(key, (prevQueryData) => {
+                if (prevQueryData) {
+                    if (!prevQueryData.find((artId) => artId === data.id)) {
+                        return [...prevQueryData, data.id];
+                    }
+                }
+
+                return prevQueryData;
+            });
+
+            return { prevQueryData };
+        },
+        onError: (_err, _data, ctx) => {
+            queryClient.setQueryData(key, ctx!.prevQueryData);
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries(key);
+        },
+    });
+};
