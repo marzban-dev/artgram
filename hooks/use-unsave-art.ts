bbca2902a9e798539d186ceb6622ac75f7c145c6
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-11 22:42:54 +0330

    Add use-unsave-art hook

---
+++ hooks/use-unsave-art.ts
@@ -0,0 +1,28 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { unsaveArt } from "api/user.api";
+
+export const useUnsaveArtMutation = () => {
+    const queryClient = useQueryClient();
+    const key = ["user-saves"];
+
+    return useMutation({
+        mutationFn: unsaveArt,
+        onMutate: async (data) => {
+            await queryClient.cancelQueries();
+            const prevQueryData = queryClient.getQueryData(key);
+
+            queryClient.setQueryData<number[]>(key, (prevQueryData) => {
+                if (prevQueryData) return prevQueryData.filter((artId) => artId !== data.id);
+                return prevQueryData;
+            });
+
+            return { prevQueryData };
+        },
+        onError: (_err, _data, ctx) => {
+            queryClient.setQueryData(key, ctx!.prevQueryData);
+        },
+        onSettled: () => {
+            queryClient.invalidateQueries(key);
+        },
+    });
+};
