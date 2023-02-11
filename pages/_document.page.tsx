e0149f293855a9203ee51cf697694335113dde8e
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-11 22:55:19 +0330

    Update _document component

--- pages/_document.page.tsx
+++ pages/_document.page.tsx
@@ -11,14 +11,16 @@ export default function Document() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
                 <link
-                    href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap"
+                    href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&display=swap"
                     rel="stylesheet"
                 />
             </Head>
-            <body className="font-quicksand overflow-x-hidden bg-black">
+            <body className="font-lato overflow-x-hidden bg-black">
                 <Main />
+                <div id="portal" />
                 <NextScript />
                 <script type="text/javascript" src="/assets/js/masonry.pkgd.min.js" defer></script>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/color-thief/2.3.0/color-thief.umd.js" defer></script>
             </body>
         </Html>
     );
