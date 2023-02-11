0a83fb49c53f53f0612c19de0ed137eb5454843e
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-11 22:40:19 +0330

    Update use-art-likes hook

--- hooks/use-art-likes.ts
+++ hooks/use-art-likes.ts
@@ -1,6 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import { getArtLikes } from "api/arts.api";
 
-export const useArtLikes = (id: number) => {
-    return useQuery(["art-likes", id], () => getArtLikes({ id }));
+export const useArtLikesQuery = (id: number, initialData: number = 0) => {
+    return useQuery(["art-likes", id], () => getArtLikes({ id }), {
+        refetchOnMount: false,
+        refetchOnWindowFocus: false,
+        initialData: {
+            likesCount: initialData,
+            users: [],
+        },
+    });
 };
