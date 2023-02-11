8d6f42c8ed1ae2fa9e9d35a217f64e7829393bd2
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-11 22:54:35 +0330

    Update test-utils file

--- test-utils.tsx
+++ test-utils.tsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render, RenderOptions } from "@testing-library/react";
-import { SessionProvider } from "next-auth/react";
 import { ReactElement, useRef } from "react";
 
 const Provider: React.FC<any> = ({ children }) => {
@@ -16,3 +15,4 @@ const customRender = (ui: ReactElement, options?: Omit<RenderOptions, "wrapper">
 export * from "@testing-library/jest-dom";
 export * from "@testing-library/react";
 export { customRender as render };
+
