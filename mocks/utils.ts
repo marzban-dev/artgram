bd96d515f1a78d7b2da7d1509e157bf777e9c83c
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-11 22:44:41 +0330

    Update mocks utils file

--- mocks/utils.ts
+++ mocks/utils.ts
@@ -1,7 +1,8 @@
 import { faker } from "@faker-js/faker";
 import { IArt } from "api/arts.types";
+import { IFollower, ISavedArt, IUser } from "api/user.types";
 
-export const generateMockArt = (id?: number) : IArt => {
+export const generateMockArt = (id?: number): IArt => {
     return {
         id: id ? id : faker.datatype.number({ min: 1, max: 100000 }),
         artist: {
@@ -15,5 +16,39 @@ export const generateMockArt = (id?: number) : IArt => {
         title: faker.lorem.words(),
         type: faker.lorem.words(2),
         year: faker.date.recent().toString(),
+        likes_count: faker.datatype.number({ min: 1, max: 100000 }),
+        user_like: true,
+        user_repost: true,
+    };
+};
+
+export const generateMockSavedArt = (id?: number): ISavedArt => {
+    return {
+        art: generateMockArt(),
+        created_date: faker.date.recent().toString(),
+        id: id ? id : faker.datatype.number({ min: 1, max: 100000 }),
+        owner: generateMockUser(),
+        text: faker.lorem.lines(1),
+    };
+};
+
+export const generateMockUser = (username?: string): IUser => {
+    return {
+        username: username ? username : faker.internet.userName(),
+        bio: faker.lorem.lines(),
+        date_joined: faker.date.recent().toString(),
+        first_name: faker.name.firstName(),
+        header_img: faker.internet.url(),
+        link: faker.internet.url(),
+        profile_img: faker.internet.url(),
+        location: faker.address.country(),
+    };
+};
+
+export const generateMockFollower = (username?: string): IFollower => {
+    return {
+        first_name: faker.name.firstName(),
+        profile_img: faker.internet.url(),
+        username: username ? username : faker.internet.userName(),
     };
 };
