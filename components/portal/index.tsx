199bb210f1858808677350edb1427583d8ff7f6e
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-11 22:38:10 +0330

    Create portal component   * it wraps given children with createPortal

---
+++ components/portal/index.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useRef, useState } from "react";
+import { createPortal } from "react-dom";
+import { IPortalProps } from "./portal.types";
+
+export const Portal: React.FC<IPortalProps> = ({ children }) => {
+    const ref = useRef<Element | null>(null);
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        ref.current = document.querySelector<HTMLElement>("#portal");
+        setMounted(true);
+    }, []);
+
+    return mounted && ref.current ? createPortal(children, ref.current) : null;
+};
