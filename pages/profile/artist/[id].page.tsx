9217df567a6b9d25992c0be906cab37efbf525b2
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-12 14:46:55 +0330

    Update artist page

--- pages/profile/artist/[id].page.tsx
+++ pages/profile/artist/[id].page.tsx
@@ -1,5 +1,4 @@
 import { dehydrate, QueryClient } from "@tanstack/react-query";
-import { getArtistProfile } from "api/user.api";
 import { IGetArtistProfileResponse } from "api/user.types";
 import { useArtistQuery } from "hooks/use-artist";
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
@@ -7,29 +6,25 @@ import { useRouter } from "next/router";
 import CalenderIcon from "public/assets/icon/calendar-star.svg";
 import { useMemo } from "react";
 import ProfileContainer from "../components/profile-container";
-import { IProfileInfoProps } from "../components/profile-container/components/header/components/profile-info/profile-info.types";
 
 const ArtistPage: NextPage = () => {
     const { query } = useRouter();
     const { data: artistData } = useArtistQuery(String(query.id));
 
     const profileInfo = useMemo(() => {
-        const info: IProfileInfoProps[] = [];
-
-        info.push({ icon: CalenderIcon, text: "Wikipedia", link: artistData!.wikipedia });
-        if (artistData?.age) info.push({ icon: CalenderIcon, text: artistData.age });
-        if (artistData?.artnet) info.push({ icon: CalenderIcon, text: "Artnet", link: artistData.artnet });
-        if (artistData?.googlearts) info.push({ icon: CalenderIcon, text: "Google", link: artistData.googlearts });
-
-        return info;
+        return [
+            { icon: CalenderIcon, text: artistData!.school },
+            { icon: CalenderIcon, text: artistData!.birth_data },
+            { icon: CalenderIcon, text: artistData!.profession },
+        ];
     }, [artistData]);
 
     return (
         <ProfileContainer
             name={artistData!.name}
-            avatar={artistData!.avatar}
-            description={artistData?.description}
-            background={artistData?.background}
+            avatar={artistData!.image}
+            description={artistData?.bio}
+            background={undefined}
             artsCount={artistData!.artsCount}
             profileInfo={profileInfo}
             type="artist"
@@ -52,17 +47,16 @@ export const getStaticProps: GetStaticProps = async (context) => {
     try {
         // const artistProfile = await getArtistProfile({ id: artistId });
         const artistProfile: IGetArtistProfileResponse = {
-            id: "721",
+            id: 721,
+            name: "BREGNO, Lorenzo",
+            birth_data: "(b. ca. 1480, Verona, d. 1525, Venezia)",
+            profession: "sculptor",
+            school: "Italian",
+            bio: "Italian sculptor and architect, part of family of sculptors and architects, younger brother of Giovanni Battista Bregno.\r\nHis career began in Treviso, in the workshop of Giovanni Battista. The first documentary information about him dates from 1506, when he was paid for an unidentified work for the chapel of the Holy Sacrament in Treviso Cathedral, where his brother had been working since 1504. Work began in 1507 on the urn of SS Theonestus, Tabra and Tabrata on the high altar of Treviso Cathedral. This sculpture had been commissioned in 1485 from Pietro Lombardo, but according to research it may be the earliest surviving work by Bregno. There is documentation, however, for Bregno's authorship of the Four Evangelists (1511-12) and the St Paul (1513) in the chapel of the Holy Sacrament. In these works, as indeed in the urn, there are clear references to the sculpture of Antonio Lombardo and Pietro Lombardo.\r\nOther works of the period include the damaged altar of the Holy Sepulchre (not later than 1511) in the church of San Martino in Venice. In 1512 the artist produced three statues for an altar, later dismantled, in the Venetian church of Santa Marina; in the 19th century the Magdalene and the St Catherine were placed on the monument of Doge Andrea Vendramin in Santi Giovanni e Paolo, while the statue of St Marina is at the Seminario Patriarcale. In 1515 Bregno executed sculptures for the altar of St Sebastian in the church of Santa Margherita in Treviso. These consist of a statue of the saint (now Treviso Cathedral) and a relief (now SzÃ©pmÅ±vÃ©szeti MÃºzeum, Budapest), probably by an assistant.\r\nDuring this period Bregno also produced the statues for various funerary monuments in Venice, including that of Benedetto Pesaro (Santa Maria dei Frari; the figure of Mars is by the Tuscan artist Baccio da Montelupo), as well as those of Bartolomeo Bragadin (Santi Giovanni e Paolo) and Lorenzo Gabriel, Bishop of Bergamo (Museum fÃ¼r angewandte Kunst, Vienna). This last work was dismantled after the suppression of the oratory (1810), and the life-size statue of the bishop is now in Vienna.\r\nIn 1514 Bregno obtained the commission for three statues (St Leonard, St Eustace and St Christopher) to decorate the altar of the chapel of San Leonardo in Cesena Cathedral; these were installed in February 1517. Like the preceding works, they appear stylistically related to the Lombardi. The high altar at the church of Santa Maria dei Frari, the attribution of which is disputed by critics, dates from the same period, as does the funerary monument to Bartolino da Terni (signed) in the church of the Santa TrinitÃ  in Crema. The statues in the Scuola di S Rocco, recently attributed to Bregno, and one of St John the Evangelist (Bode Museum, Berlin), can also be assigned to this period.\r\nIn 1522 Bregno worked on the funerary monument to Bertucci Lamberti in Treviso Cathedral. He was active in the Venetian church of Santa Maria Mater Domini in 1524, producing the statues of St Andrew, St Peter and St Paul, perhaps his best works, as they are least influenced by the academicism of the Lombardi. The other works in the church that are attributed to him were executed by his workshop after his death, with the help of the Paduan sculptor Antonio Minello (c. 1465-c. 1529).",
+            image: "https://www.wga.hu/biojpg/b/bregno/lorenzo/biograph.jpg",
+            wikipedia: null,
+            following: false,
             artsCount: 27,
-            name: "Jan de Bray",
-            wikipedia: "Jan de Bray",
-            avatar: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQeOA-djNbmnGsD56k2r4nKnLpLnEBsMN6TS-mWatjO3bFIQbnVnkjSBjr-n2A&s",
-            background:
-                "https://lh3.googleusercontent.com/ci/AEwo86c2wa6i0WaNp2EK8dzzOKhUg3xmguEhJ68jKicR8OlckrwNcqtOBrIEt6LQtxGpN8-nRgXXDQ",
-            age: "1627 - Apr 1, 1697",
-            description:
-                "Jan de Bray, was a Dutch Golden Age painter. He lived and worked in Haarlem until the age of 60, when he went bankrupt and moved to Amsterdam.<br>Jan de Bray was influenced by his father Salomon de Bray, and the portraitists Bartholomeus van der Helst, and Frans Hals. De Bray's works are mainly portraits, often of groups, and history paintings. He specialised in combining the two genres in the portrait historié, portraits of historical figures using contemporary figures, including himself and his family. Among his finest works are two versions of the Banquet of Cleopatra, using his own family, including himself, as models. The second version has great pathos, as most of those depicted had died in the plague of 1663–4.",
-            googlearts: "https://artsandculture.google.com/entity/jan-de-bray/m09pmd3?categoryId=artist",
         };
 
         await queryClient.prefetchQuery(["artist", artistId], () => artistProfile);
