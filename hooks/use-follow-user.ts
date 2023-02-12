3135236c58b0a1016e90165de327d64a92547907
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-13 00:55:06 +0330

    Update use-follow-user hook

--- hooks/use-follow-user.ts
+++ hooks/use-follow-user.ts
@@ -1,28 +1,6 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { followUser } from "api/user.api";
 
-export const useFollowUserMutation = (id: number | string) => {
-    const queryClient = useQueryClient();
-    const key = ["user-followers", id];
-
-    return useMutation({
-        mutationFn: followUser,
-        onMutate: async () => {
-            await queryClient.cancelQueries();
-            const prevQueryData = queryClient.getQueryData(key);
-
-            queryClient.setQueryData<number>(key, (prevQueryData) => {
-                if (prevQueryData) return prevQueryData - 1;
-                return prevQueryData;
-            });
-
-            return { prevQueryData };
-        },
-        onError: (_err, _data, ctx) => {
-            queryClient.setQueryData(key, ctx!.prevQueryData);
-        },
-        onSettled: () => {
-            queryClient.invalidateQueries(key);
-        },
-    });
+export const useFollowUserMutation = () => {
+    return useMutation({ mutationFn: followUser });
 };
