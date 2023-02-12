cc8dfdbc3b73fb74509b6baf25c0aea68909e349
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-13 00:54:49 +0330

    Update use-follow-user test file

--- hooks/__tests__/use-follow-user.test.tsx
+++ hooks/__tests__/use-follow-user.test.tsx
@@ -1,11 +1,10 @@
 import { useFollowUserMutation } from "hooks/use-follow-user";
-import { useUnlikeArtMutation } from "hooks/use-unlike-art";
 import { renderHook, waitFor } from "test-utils";
 import TestWrapper from "utils/test-wrapper";
 
 describe("useFollowUser", () => {
     test("should respond correctly", async () => {
-        const { result } = renderHook(() => useFollowUserMutation(1), {
+        const { result } = renderHook(() => useFollowUserMutation(), {
             wrapper: TestWrapper,
         });
 
