7ddf23eeb29f8c5f550016e471132dbdd875e163
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-13 00:56:10 +0330

    Update art page

--- pages/art/[id].page.tsx
+++ pages/art/[id].page.tsx
@@ -1,4 +1,5 @@
 import { dehydrate, QueryClient } from "@tanstack/react-query";
+import { getArt } from "api/arts.api";
 import { useArtQuery } from "hooks/use-art";
 import PageTransition from "layouts/page-transition";
 import { GetServerSideProps, NextPage } from "next";
@@ -43,35 +44,36 @@ export const getServerSideProps: GetServerSideProps<any, TArtPageUrlParams> = as
     const artId = Number(context.params!.id);
 
     try {
-        // const art = await getArt({ id: artId, token: session?.accessToken });
+        const art = await getArt({ id: artId, token: session?.accessToken });
         await queryClient.prefetchQuery(["art", artId], () => {
-            return {
-                id: 7306,
-                artist: {
-                    id: 909,
-                    name: "CAPORALI, Bartolomeo",
-                    image: "https://www.wga.hu/biojpg/c/caporali/biograph.jpg",
-                    following: false,
-                },
-                title: "Annunciation",
-                date: "1467-68",
-                technique: "Tempera, oil and gold on panel, 50 x 48 cm (each)",
-                location: "Galleria Nazionale dell'Umbria, Perugia",
-                image: {
-                    id: 7306,
-                    url: "https://www.wga.hu/art/c/caporali/annuncia.jpg",
-                    width: 1629,
-                    height: 800,
-                    thumbnail: "https://www.wga.hu/preview/c/caporali/annuncia.jpg",
-                },
-                form: "painting",
-                school: "Italian",
-                type: "religious",
-                reference: "www.wga.hu",
-                likes_count: 1,
-                user_like: true,
-                user_repost: false,
-            };
+            return art;
+            // return {
+            //     id: 7306,
+            //     artist: {
+            //         id: 909,
+            //         name: "CAPORALI, Bartolomeo",
+            //         image: "https://www.wga.hu/biojpg/c/caporali/biograph.jpg",
+            //         following: false,
+            //     },
+            //     title: "Annunciation",
+            //     date: "1467-68",
+            //     technique: "Tempera, oil and gold on panel, 50 x 48 cm (each)",
+            //     location: "Galleria Nazionale dell'Umbria, Perugia",
+            //     image: {
+            //         id: 7306,
+            //         url: "https://www.wga.hu/art/c/caporali/annuncia.jpg",
+            //         width: 1629,
+            //         height: 800,
+            //         thumbnail: "https://www.wga.hu/preview/c/caporali/annuncia.jpg",
+            //     },
+            //     form: "painting",
+            //     school: "Italian",
+            //     type: "religious",
+            //     reference: "www.wga.hu",
+            //     likes_count: 1,
+            //     user_like: true,
+            //     user_repost: false,
+            // };
         });
     } catch (e) {
         return {
