0334f9d521486579c1b57acd8ddb45889658cf8d
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-13 00:15:48 +0330

    Update use-art-likes hook

--- hooks/use-art-likes.ts
+++ hooks/use-art-likes.ts
@@ -3,8 +3,6 @@ import { getArtLikes } from "api/arts.api";
 
 export const useArtLikesQuery = (id: number, initialData: number = 0) => {
     return useQuery(["art-likes", id], () => getArtLikes({ id }), {
-        refetchOnMount: false,
-        refetchOnWindowFocus: false,
         initialData: {
             likesCount: initialData,
             users: [],
