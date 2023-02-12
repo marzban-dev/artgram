2060c83f08a033b2cb28f3f109bf438570cfc5b4
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-13 00:15:34 +0330

    Update modal component

--- components/modal/index.tsx
+++ components/modal/index.tsx
@@ -67,7 +67,7 @@ const Modal: React.FC<IModalProps> = ({ show, onClose, children }) => {
                 {show && (
                     <div className="z-[999] fixed top-0 flex justify-center items-center w-full h-full [perspective:1000px]">
                         <motion.div
-                            className="z-[1000] min-h-[250px] max-h-[500px] h-full w-full min-[520px]:w-[500px] rounded-t-[20px] rounded-b-none min-[520px]:rounded-[20px] bg-[rgb(20,20,20)] absolute max-[520px]:bottom-0 origin-bottom overflow-hidden"
+                            className="z-[1000] min-h-[300px] max-h-[500px] w-full min-[520px]:w-[500px] rounded-t-[20px] rounded-b-none min-[520px]:rounded-[20px] bg-[rgb(20,20,20)] absolute max-[520px]:bottom-0 origin-bottom overflow-hidden"
                             dragConstraints={{ bottom: 0, top: 0 }}
                             dragElastic={{ bottom: 0.4, top: 0.01 }}
                             dragTransition={{ bounceStiffness: 200, bounceDamping: 25 }}
