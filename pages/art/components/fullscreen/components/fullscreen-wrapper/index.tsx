789fbd2e116d6c2976798543743a434f141aa619
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-13 21:50:18 +0330

    Update fullscreen component   * add loading statement

--- pages/art/components/fullscreen/components/fullscreen-wrapper/index.tsx
+++ pages/art/components/fullscreen/components/fullscreen-wrapper/index.tsx
@@ -1,3 +1,4 @@
+import Spinner from "components/spinner";
 import { motion, useSpring, useTransform, Variants } from "framer-motion";
 import Image from "next/image";
 import SampleArt from "public/assets/img/arts/18.jpg";
@@ -9,6 +10,7 @@ import { IFullscreenWrapperProps } from "./fullscreen-wrapper.types";
 const FullscreenWrapper: React.FC<IFullscreenWrapperProps> = ({ fullscreenArt }) => {
     const dispatch = useDispatch();
     const artPostContainer = useRef<HTMLDivElement | null>(null);
+    const [isLoaded, setIsLoaded] = useState(false);
     const [containerWidth, setContainerWidth] = useState(1);
     const [containerHeight, setContainerHeight] = useState(1);
 
@@ -90,7 +92,7 @@ const FullscreenWrapper: React.FC<IFullscreenWrapperProps> = ({ fullscreenArt })
 
     return (
         <motion.div
-            className="fixed z-[100] top-0 left-0 w-full h-full bg-[rgba(10,10,10,1)] [perspective:1000px] cursor-pointer"
+            className="fixed z-[100] top-0 left-0 w-full h-full bg-[rgb(0,0,0)] [perspective:1000px] cursor-pointer"
             ref={artPostContainer}
             onClick={exitFullscreen}
             onMouseMove={handleMouseMove}
@@ -100,7 +102,13 @@ const FullscreenWrapper: React.FC<IFullscreenWrapperProps> = ({ fullscreenArt })
             exit="hide"
             title="Exit fullscreen"
         >
-            <motion.div className="w-full h-full flex justify-center items-center [perspective:1000px]" variants={wrapperVariants} initial="hide" animate="show" exit="exit">
+            <motion.div
+                className="w-full h-full flex justify-center items-center [perspective:1000px]"
+                variants={wrapperVariants}
+                initial="hide"
+                animate="show"
+                exit="exit"
+            >
                 <motion.div
                     className="w-full h-full max-h-[90vh] max-w-[90vw] relative"
                     style={{
@@ -108,7 +116,23 @@ const FullscreenWrapper: React.FC<IFullscreenWrapperProps> = ({ fullscreenArt })
                         rotateX: transformedMouseY,
                     }}
                 >
-                    <Image src={SampleArt} alt={fullscreenArt.title} quality={100} className="rounded-[25px]" style={{ objectFit: "contain" }} priority fill />
+                    <motion.div
+                        animate={{ opacity: isLoaded ? 0 : 1 }}
+                        className="fixed w-full h-full bg-black left-0 top-0 flex justify-center items-center flex-col z-[1000] gap-4"
+                    >
+                        <Spinner size={50} />
+                        <span className="text-white text-[18px]">Fullscreen Loading</span>
+                    </motion.div>
+                    <Image
+                        src={fullscreenArt.picture}
+                        alt={fullscreenArt.title}
+                        quality={100}
+                        className="rounded-[25px]"
+                        style={{ objectFit: "contain" }}
+                        onLoadingComplete={() => setIsLoaded(true)}
+                        priority
+                        fill
+                    />
                 </motion.div>
             </motion.div>
         </motion.div>
