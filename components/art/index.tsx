e9c9a14abdc53020981284f491076583bf6a85a2
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-13 13:49:21 +0330

    Update art component

--- components/art/index.tsx
+++ components/art/index.tsx
@@ -1,32 +1,21 @@
-import Like from "components/like";
-import Placeholder from "components/placeholder";
-import { AnimatePresence, motion, useInView, Variants } from "framer-motion";
+import { motion, useInView, Variants } from "framer-motion";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { IArtProps } from "./art.types";
+import ActionOverlay from "./components/action-overlay";
+import Placeholder from "./components/placeholder";
 
-const Art: React.FC<IArtProps> = ({ id, picture, title, likes_count, user_like }) => {
+const Art: React.FC<IArtProps> = ({ id, image, title, user_like }) => {
     const router = useRouter();
     const [isLoaded, setIsLoaded] = useState(false);
     const [isLiked, setIsLiked] = useState(user_like);
     const [showActions, setShowActions] = useState(isLiked);
-    const [imageHeight, setImageHeight] = useState(0);
 
     const imageContainerRef = useRef<HTMLElement | null>(null);
-    const imagePictureRef = useRef<HTMLImageElement | null>(null);
     const isImageInView = useInView(imageContainerRef, { once: true });
 
-    useEffect(() => {
-        const imageElement = imagePictureRef.current!;
-
-        const parentWidth = imageContainerRef.current!.clientWidth;
-        const imageAspectRatio = imageElement.naturalHeight / imageElement.naturalWidth;
-        console.log(imageElement.naturalHeight);
-        setImageHeight(parentWidth * imageAspectRatio);
-    }, []);
-
-    const imageVariants: Variants = {
+    const imageWrapperVariants: Variants = {
         hide: {
             opacity: 0,
             rotateX: -40,
@@ -39,26 +28,6 @@ const Art: React.FC<IArtProps> = ({ id, picture, title, likes_count, user_like }
                 type: "spring",
                 stiffness: 200,
                 damping: 40,
-            },
-        },
-    };
-
-    const actionsVariants: Variants = {
-        hide: {
-            opacity: 0,
-            scale: 0.9,
-            transition: {
-                duration: 0.3,
-                opacity: {
-                    duration: 0.2,
-                },
-            },
-        },
-        show: {
-            opacity: 1,
-            scale: 1,
-            transition: {
-                duration: 0.2,
             },
         },
     };
@@ -76,42 +45,29 @@ const Art: React.FC<IArtProps> = ({ id, picture, title, likes_count, user_like }
                 <motion.div
                     id={`art-${id}`}
                     initial="hide"
-                    variants={imageVariants}
+                    variants={imageWrapperVariants}
                     animate={isImageInView ? "show" : "hide"}
-                    className="rounded-[25px] overflow-hidden origin-top"
+                    className="rounded-[25px] overflow-hidden origin-top relative"
                 >
-                    <AnimatePresence>{!isLoaded && <Placeholder width="100%" height={imageHeight} />}</AnimatePresence>
-
+                    <Placeholder isLoaded={isLoaded} />
                     <motion.div
                         animate={{ opacity: isLoaded ? 1 : 0 }}
                         className="rounded-[25px] overflow-hidden relative w-full h-full"
                     >
-                        <motion.div
-                            animate={{ opacity: showActions ? 1 : 0, scale: 1 }}
-                            transition={{duration : 0.2}}
-                            className="h-full w-full bg-art-like-button-gradient absolute bottom-0 right-0"
+                        <ActionOverlay
+                            id={id}
+                            showActions={showActions}
+                            setIsLiked={setIsLiked}
+                            user_like={user_like}
                         />
-                        <motion.div
-                            variants={actionsVariants}
-                            animate={showActions ? "show" : "hide"}
-                            className="absolute bottom-[10px] right-[20px]"
-                        >
-                            <Like
-                                id={id}
-                                initial={user_like}
-                                onLikeCallback={() => setIsLiked(true)}
-                                onDislikeCallback={() => setIsLiked(false)}
-                            />
-                        </motion.div>
                         <Image
-                            ref={imagePictureRef}
                             onLoadingComplete={() => setIsLoaded(true)}
-                            src={picture}
-                            quality={25}
+                            src={image.url}
+                            quality={50}
                             alt={title}
                             title={title}
-                            width={400}
-                            height={700}
+                            width={image.width}
+                            height={image.height}
                         />
                     </motion.div>
                 </motion.div>
@@ -119,4 +75,5 @@ const Art: React.FC<IArtProps> = ({ id, picture, title, likes_count, user_like }
         </section>
     );
 };
+
 export default Art;
