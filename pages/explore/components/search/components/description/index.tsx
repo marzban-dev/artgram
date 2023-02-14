0790a9cb8bc34ac75deb292aa74aa6bc6164f2eb
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-14 22:57:09 +0330

    Create description component

---
+++ pages/explore/components/search/components/description/index.tsx
@@ -0,0 +1,15 @@
+const Description: React.FC = () => {
+    return (
+        <div className="z-20 flex justify-center items-center flex-col">
+            <h1 className="text-white text-[40px] font-bold w-[500px] text-center [line-height:1.2]">
+                Explore the world's leading design portfolios
+            </h1>
+            <h2 className="text-[rgb(210,210,210)] text-[18px] text-center mt-5 mb-10 w-[700px]">
+                Millions of designers and agencies around the world showcase their portfolio work on Dribbble - the home
+                to the world’s best design and creative professionals.
+            </h2>
+        </div>
+    );
+};
+
+export default Description;
