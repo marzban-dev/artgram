a6dd62694773f5ec534a649da2c4b9a52d3fb725
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-14 22:51:08 +0330

    Create use-search hook

---
+++ hooks/use-search.ts
@@ -0,0 +1,35 @@
+import { useMemo } from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { getArts } from "api/arts.api";
+import { useSession } from "next-auth/react";
+import { useSelector } from "react-redux";
+import { RootState } from "store";
+
+export const useSearch = () => {
+    const search = useSelector((state: RootState) => state.explore.search);
+    const searchBy = useSelector((state: RootState) => state.explore.searchBy);
+    const orderBy = useSelector((state: RootState) => state.explore.orderBy);
+    const { data } = useSession();
+
+    const limit = 15;
+
+    const pageParamDefaults = {
+        ordering: `${orderBy === "des" ? "-" : ""}${searchBy}`,
+        [`${searchBy}__contains`]: search,
+        limit,
+        offset: 0,
+        token: data?.accessToken,
+    };
+
+    return useInfiniteQuery(["search"], ({ pageParam = pageParamDefaults }) => getArts({ pageParam }), {
+        enabled: !!search,
+        getNextPageParam: (_lastPage, pages) => {
+            const page = pages.length + 1;
+
+            return {
+                ...pageParamDefaults,
+                offset: limit * page - limit,
+            };
+        },
+    });
+};
