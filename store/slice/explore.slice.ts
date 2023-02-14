8d5c9d42498dfd6f21e4e1da2f8e0a898f155bbc
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-14 22:52:49 +0330

    Update explore slice

--- store/slice/explore.slice.ts
+++ store/slice/explore.slice.ts
@@ -1,23 +1,36 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
+export type TSearchBy = "title" | "date" | "technique" | "form" | "school" | "type" | "location";
+export type TOrderBy = "asc" | "des";
+
 interface IInitialState {
-    searchBy: string;
+    search: string | null;
+    searchBy: TSearchBy;
+    orderBy: TOrderBy;
 }
 
 const init: IInitialState = {
+    search: null,
     searchBy: "title",
+    orderBy: "asc",
 };
 
 export const exploreSlice = createSlice({
     name: "explore",
     initialState: init,
     reducers: {
-        setSearchBy(state, action: PayloadAction<string>) {
+        setSearch(state, action: PayloadAction<string | null>) {
+            state.search = action.payload;
+        },
+        setSearchBy(state, action: PayloadAction<TSearchBy>) {
             state.searchBy = action.payload;
+        },
+        setOrderBy(state, action: PayloadAction<TOrderBy>) {
+            state.orderBy = action.payload;
         },
     },
 });
 
-export const { setSearchBy } = exploreSlice.actions;
+export const { setSearchBy, setOrderBy, setSearch } = exploreSlice.actions;
 
 export default exploreSlice.reducer;
