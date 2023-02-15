2a85794a101ac182055b9390d97f12913ffc15a6
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-15 13:36:18 +0330

    Update page-padding layout

--- layouts/page-padding/index.tsx
+++ layouts/page-padding/index.tsx
@@ -2,7 +2,7 @@ import { IPagePaddingProps } from "./page-padding.types";
 
 const PagePadding: React.FC<IPagePaddingProps> = ({ children }) => {
     return (
-        <div className="w-full h-full px-[12%]">
+        <div className="w-full h-full px-[6%] min-[550px]:px-[8%] min-[750px]:px-[12%]">
             {children}
         </div>
     );
