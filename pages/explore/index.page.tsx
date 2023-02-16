bd9314cf0fcd949058085d3b45797b567b218b74
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-16 18:08:13 +0330

    Update explore page

--- pages/explore/index.page.tsx
+++ pages/explore/index.page.tsx
@@ -1,23 +1,22 @@
 import { dehydrate, QueryClient } from "@tanstack/react-query";
 import { getArts } from "api/arts.api";
-import InfiniteArts from "components/infinite-arts";
-import { useExploreQuery } from "hooks/use-explore";
 import PagePadding from "layouts/page-padding";
 import PageTransition from "layouts/page-transition";
 import { GetServerSideProps, NextPage } from "next";
 import Navbar from "./components/navbar";
 import Search from "./components/search";
+import ShowExplore from "./components/show-explore";
+import ShowSearch from "./components/show-search";
 
 const ExplorePage: NextPage = () => {
-    const { data: arts, fetchNextPage } = useExploreQuery();
-
     return (
         <PageTransition>
             <main className="h-screen">
                 <Navbar />
                 <Search />
                 <PagePadding>
-                    {arts && <InfiniteArts arts={arts.pages.flat()} callback={fetchNextPage} count={Infinity} />}
+                    <ShowExplore />
+                    <ShowSearch />
                 </PagePadding>
             </main>
         </PageTransition>
@@ -30,7 +29,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     const isRequestFromRouter = context.req.url?.includes("_next");
 
     if (!isRequestFromRouter) {
-        await queryClient.prefetchInfiniteQuery(["explore"], () => getArts({ pageParam: { limit: 10 } }));
+        await queryClient.prefetchInfiniteQuery(["explore"], () => getArts({ pageParam: { limit: 15 } }));
 
         return {
             props: {
