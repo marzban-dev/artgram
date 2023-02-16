ac753503773b6a85db86cbfa17ceb47c59b7210c
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-16 18:09:10 +0330

    Update search component

--- pages/explore/components/search/index.tsx
+++ pages/explore/components/search/index.tsx
@@ -1,19 +1,14 @@
-import { Fragment } from "react";
 import Background from "./components/background";
 import Description from "./components/description";
-import FilterModal from "./components/filter-modal";
 import SearchInput from "./components/search-input";
 
 const Search: React.FC = () => {
     return (
-        <Fragment>
-            <div className="w-full h-[65vh] flex justify-center items-center flex-col overflow-hidden relative mb-8">
-                <Background />
-                <Description />
-                <SearchInput />
-            </div>
-            <FilterModal />
-        </Fragment>
+        <div className="w-full h-[65vh] flex justify-center items-center flex-col overflow-hidden relative mb-8">
+            <Background />
+            <Description />
+            <SearchInput />
+        </div>
     );
 };
 
