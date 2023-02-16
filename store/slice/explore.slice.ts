3d92422fc146a38649cb01c11898932817880633
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-16 18:06:07 +0330

    Update explore slice   * add isSearching state and create set action for that

--- store/slice/explore.slice.ts
+++ store/slice/explore.slice.ts
@@ -4,12 +4,14 @@ export type TSearchBy = "title" | "date" | "technique" | "form" | "school" | "ty
 export type TOrderBy = "asc" | "des";
 
 interface IInitialState {
+    isSearching: boolean;
     search: string | null;
     searchBy: TSearchBy;
     orderBy: TOrderBy;
 }
 
 const init: IInitialState = {
+    isSearching: false,
     search: null,
     searchBy: "title",
     orderBy: "asc",
@@ -19,6 +21,9 @@ export const exploreSlice = createSlice({
     name: "explore",
     initialState: init,
     reducers: {
+        setIsSearching(state, action: PayloadAction<boolean>) {
+            state.isSearching = action.payload;
+        },
         setSearch(state, action: PayloadAction<string | null>) {
             state.search = action.payload;
         },
@@ -31,6 +36,6 @@ export const exploreSlice = createSlice({
     },
 });
 
-export const { setSearchBy, setOrderBy, setSearch } = exploreSlice.actions;
+export const { setIsSearching, setSearchBy, setOrderBy, setSearch } = exploreSlice.actions;
 
 export default exploreSlice.reducer;
