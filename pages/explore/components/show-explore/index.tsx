a9cfb6af63c9dd78f50c9c9331fb34a00e1a1e9f
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-16 18:07:43 +0330

    Create show-explore component

---
+++ pages/explore/components/show-explore/index.tsx
@@ -0,0 +1,18 @@
+import InfiniteArts from "components/infinite-arts";
+import { useExploreQuery } from "hooks/use-explore";
+import { useSearchQuery } from "hooks/use-search";
+import { useSelector } from "react-redux";
+import { RootState } from "store";
+
+const ShowExplore: React.FC = () => {
+    const { data: arts, fetchNextPage: fetchNextExplorePage } = useExploreQuery();
+
+    const { data: searchResult } = useSearchQuery();
+    const isSearching = useSelector((state: RootState) => state.explore.isSearching);
+
+    return !searchResult && !isSearching ? (
+        <InfiniteArts arts={arts!.pages.flat()} callback={fetchNextExplorePage} count={Infinity} />
+    ) : null;
+};
+
+export default ShowExplore;
