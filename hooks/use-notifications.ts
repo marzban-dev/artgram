ae51603d464c072641930852ba84a57371c8bee7
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-20 00:05:42 +0330

    Create use-notifications hook

---
+++ hooks/use-notifications.ts
@@ -0,0 +1,19 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { getNotifications, getSavedArts } from "api/user.api";
+import { useSession } from "next-auth/react";
+
+export const useNotificationsQuery = () => {
+    const { data } = useSession();
+    const pageParamDefaults = { limit: 5, page: 1, token: data?.accessToken };
+
+    return useInfiniteQuery(["notifications"], ({ pageParam = pageParamDefaults }) => getNotifications({ pageParam }), {
+        getNextPageParam: (lastPage, pages) => {
+            const nextPage = {
+                ...pageParamDefaults,
+                page: pages.length + 1,
+            };
+
+            return lastPage.next ? nextPage : undefined;
+        },
+    });
+};
