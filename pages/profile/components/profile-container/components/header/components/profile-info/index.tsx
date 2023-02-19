24f9bb16ac7b58ef839ba7990d88ce0d82571018
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-20 00:13:47 +0330

    Update profile-info component

--- pages/profile/components/profile-container/components/header/components/profile-info/index.tsx
+++ pages/profile/components/profile-container/components/header/components/profile-info/index.tsx
@@ -1,13 +1,32 @@
+import useImageColors from "hooks/use-image-colors";
+import { useEffect } from "react";
+import setCssVariables from "utils/set-css-variables";
 import { IProfileInfoProps } from "./profile-info.types";
 
 const ProfileInfo: React.FC<IProfileInfoProps> = ({ icon: Icon, text, link, iconSize = 18 }) => {
+    const imageColors = useImageColors(`#artist-background`, {
+        darkPrimary: 0.2,
+        // darkLighter: 0.6,
+        lightPrimary: 0.1,
+        // lightLighter: 0.2,
+    });
+
+    useEffect(() => {
+        if (imageColors) {
+            setCssVariables(`#profile-container`, [
+                ["--artist-primary-color", imageColors.primary],
+                ["--artist-lighter-color", imageColors.lighter],
+            ]);
+        }
+    }, [imageColors]);
+
     const iconStyle: React.CSSProperties = {
         height: iconSize,
     };
 
     return (
         <span className="text-white flex justify-start items-center gap-2">
-            <Icon className="fill-white" style={iconStyle} />
+            <Icon className="fill-artist-primary" style={iconStyle} />
             {link ? (
                 <a
                     target="_blank"
