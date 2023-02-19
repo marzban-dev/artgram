43dca3562c0d23a89320043f85e1f7474e131537
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-20 00:05:56 +0330

    Update use-saved-arts hook

--- hooks/use-saved-arts.ts
+++ hooks/use-saved-arts.ts
@@ -1,15 +1,17 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { getSavedArts } from "api/user.api";
 
-export const useSavedArtsQuery = (id: number) => {
+export const useSavedArtsQuery = (id: string) => {
     const pageParamDefaults = { owner: id, limit: 8, page: 1 };
 
     return useInfiniteQuery(["user-saves"], ({ pageParam = pageParamDefaults }) => getSavedArts({ pageParam }), {
-        getNextPageParam: (_lastPage, pages) => {
-            return {
+        getNextPageParam: (lastPage, pages) => {
+            const nextPage = {
                 ...pageParamDefaults,
                 page: pages.length + 1,
             };
+
+            return lastPage.length !== 0 ? nextPage : undefined;
         },
     });
 };
