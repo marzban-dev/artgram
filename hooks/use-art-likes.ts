9a012ce626251a714c5a3ca0b994a3117bbdc188
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-20 00:03:29 +0330

    Update use-art-likes component   * change it to useInfiniteQuery

--- hooks/use-art-likes.ts
+++ hooks/use-art-likes.ts
@@ -1,11 +1,21 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { getArtLikes } from "api/arts.api";
 
 export const useArtLikesQuery = (id: number, initialData: number = 0) => {
-    return useQuery(["art-likes", id], () => getArtLikes({ id }), {
-        initialData: {
-            likesCount: initialData,
-            users: [],
+    const pageParamDefaults = { id };
+
+    return useInfiniteQuery(["art-likes", id], ({ pageParam = pageParamDefaults }) => getArtLikes({ pageParam }), {
+        placeholderData: {
+            pages: [{ items: [], next: "", count: initialData }],
+            pageParams: [null],
+        },
+        getNextPageParam: (lastPage, pages) => {
+            const nextPage = {
+                ...pageParamDefaults,
+                page: pages.length + 1,
+            };
+
+            return lastPage.next ? nextPage : undefined;
         },
     });
 };
