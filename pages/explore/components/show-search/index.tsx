18a44ec7cef08780ea0329e947440a5a70b28007
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-20 16:18:28 +0330

    Update show-search component

--- pages/explore/components/show-search/index.tsx
+++ pages/explore/components/show-search/index.tsx
@@ -1,18 +1,24 @@
+import { IArt } from "api/arts.types";
 import InfiniteArts from "components/infinite-arts";
 import Spinner from "components/spinner";
 import { useSearchQuery } from "hooks/use-search";
 import { Fragment } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "store";
+import flatInfiniteQueryData from "utils/flat-infinite-query-data";
 
 const ShowSearch: React.FC = () => {
     const isSearching = useSelector((state: RootState) => state.explore.isSearching);
-    const { data: searchResult, fetchNextPage: fetchNextSearchPage } = useSearchQuery();
+    const { data: searchResult, fetchNextPage: fetchNextSearchPage, hasNextPage } = useSearchQuery();
 
     return (
         <Fragment>
             {searchResult && !isSearching && (
-                <InfiniteArts arts={searchResult.pages.flat()} callback={fetchNextSearchPage} count={Infinity} />
+                <InfiniteArts
+                    arts={flatInfiniteQueryData<IArt>(searchResult)}
+                    callback={fetchNextSearchPage}
+                    hasNextPage={hasNextPage}
+                />
             )}
             {isSearching && (
                 <div className="w-full flex flex-col justify-center items-center gap-4">
