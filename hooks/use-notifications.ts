cf1b0f21b3aacd1a872f3dbde032a175fd06fc4f
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-20 16:17:33 +0330

    Update use-notifications hook

--- hooks/use-notifications.ts
+++ hooks/use-notifications.ts
@@ -2,11 +2,12 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { getNotifications, getSavedArts } from "api/user.api";
 import { useSession } from "next-auth/react";
 
-export const useNotificationsQuery = () => {
+export const useNotificationsQuery = ({ enabled }: { enabled: boolean }) => {
     const { data } = useSession();
     const pageParamDefaults = { limit: 5, page: 1, token: data?.accessToken };
 
     return useInfiniteQuery(["notifications"], ({ pageParam = pageParamDefaults }) => getNotifications({ pageParam }), {
+        enabled,
         getNextPageParam: (lastPage, pages) => {
             const nextPage = {
                 ...pageParamDefaults,
