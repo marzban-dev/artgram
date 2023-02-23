a0f63b85a9ca3940e3babe4b3e1ad7f7c7a7b487
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-23 15:16:50 +0330

    Update user component

--- components/user/index.tsx
+++ components/user/index.tsx
@@ -1,14 +1,16 @@
 import Avatar from "components/avatar";
+import FollowButton from "components/follow-button";
 import { IUserProps } from "./user.types";
 
 const User: React.FC<IUserProps> = ({ avatar, firstName, username }) => {
     return (
         <div className="w-full flex justify-start items-center gap-4 h-[60px]">
             <Avatar width={60} height={60} picture={avatar} title={username} />
-            <div className="h-full flex flex-col justify-center items-start text-white">
+            <div className="h-full w-[calc(100%-190px)] flex flex-col justify-center items-start text-white">
                 <span className="text-[18px]">{firstName}</span>
                 <span className="text-[rgb(150,150,150)] text-[16px]">{username}</span>
             </div>
+            <FollowButton id={username} type="user" width={100} colorClass="profile" initial={false} />
         </div>
     );
 };
