cc4edc0906215766c5e5b6471638cd3bacdfeefc
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-23 15:21:29 +0330

    Update use-saved-arts hook

--- hooks/use-saved-arts.ts
+++ hooks/use-saved-arts.ts
@@ -11,7 +11,7 @@ export const useSavedArtsQuery = (id: string) => {
                 page: pages.length + 1,
             };
 
-            return lastPage.length !== 0 ? nextPage : undefined;
+            return lastPage.next ? nextPage : undefined;
         },
     });
 };
