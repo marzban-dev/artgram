4970b8436853c2b452a51ec628df5dfd02df131c
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-23 15:21:05 +0330

    Update use-arts hook

--- hooks/use-arts.ts
+++ hooks/use-arts.ts
@@ -1,10 +1,8 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { getArts } from "api/arts.api";
-import { useSession } from "next-auth/react";
 
 export const useArtsQuery = (id: number) => {
-    const { data } = useSession();
-    const pageParamDefaults = { limit: 5, token: data?.accessToken };
+    const pageParamDefaults = { limit: 5 };
 
     return useInfiniteQuery(["arts", id], ({ pageParam = pageParamDefaults }) => getArts({ pageParam }), {
         cacheTime: Infinity,
