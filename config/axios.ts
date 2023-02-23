52322fe46d031856ebaecc5e9a8c93e60059ed40
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-23 15:17:08 +0330

    Update axios config file

--- config/axios.ts
+++ config/axios.ts
@@ -1,7 +1,18 @@
 import axiosModule from "axios";
+import createAuthHeader from "utils/create-auth-header";
 
 const axios = axiosModule.create({
     baseURL: "https://artgram.iran.liara.run",
+});
+
+axios.interceptors.request.use(async (config) => {
+    if (!config.url!.includes("/auth/")) {
+        const authHeader = await createAuthHeader();
+        config.headers!.Authorization = authHeader.Authorization;
+        return config;
+    }
+
+    return config;
 });
 
 export default axios;
