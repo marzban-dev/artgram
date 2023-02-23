02963154ee38ba984b8037b32cdad7ceca53dcc0
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-23 15:34:50 +0330

    Update user-arts component

--- pages/profile/components/profile-container/components/user-arts/index.tsx
+++ pages/profile/components/profile-container/components/user-arts/index.tsx
@@ -1,14 +1,20 @@
+import { ISavedArt } from "api/user.types";
 import InfiniteArts from "components/infinite-arts";
 import Spinner from "components/spinner";
 import { useSavedArtsQuery } from "hooks/use-saved-arts";
 import React, { useMemo } from "react";
+import flatInfiniteQueryData from "utils/flat-infinite-query-data";
 import { IUserArtsProps } from "./user-arts.types";
 
 const UserArts: React.FC<IUserArtsProps> = ({ id }) => {
     const { data: savedArts, fetchNextPage, hasNextPage } = useSavedArtsQuery(id);
 
     const arts = useMemo(() => {
-        return savedArts?.pages.flat().map((savedArt) => savedArt.art);
+        if (savedArts) {
+            return flatInfiniteQueryData<ISavedArt>(savedArts).map((savedArt) => savedArt.art);
+        }
+
+        return null;
     }, [savedArts]);
 
     return arts ? (
