22d84e90fee6202e928a74022df6b95bb22f08af
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-23 15:22:42 +0330

    Update create-auth-header util file

--- utils/create-auth-header.ts
+++ utils/create-auth-header.ts
@@ -1,7 +1,15 @@
-const createAuthHeader = (token: string) => {
-    return {
-        Authorization: `Bearer ${token}`,
-    };
+import { getSession } from "next-auth/react";
+
+const createAuthHeader = async () => {
+    const session = await getSession();
+
+    if (session) {
+        return {
+            Authorization: `Bearer ${session.accessToken}`,
+        };
+    }
+
+    return {};
 };
 
 export default createAuthHeader;
