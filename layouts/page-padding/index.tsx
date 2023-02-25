dcced746c584e5fb4b756db210285f79fa818f6d
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-25 16:52:23 +0330

    Update page-padding layout

--- layouts/page-padding/index.tsx
+++ layouts/page-padding/index.tsx
@@ -1,11 +1,17 @@
+import classNames from "classnames";
+import { useRouter } from "next/router";
 import { IPagePaddingProps } from "./page-padding.types";
 
 const PagePadding: React.FC<IPagePaddingProps> = ({ children }) => {
-    return (
-        <div className="w-full h-full px-[6%] min-[550px]:px-[8%] min-[750px]:px-[12%]">
-            {children}
-        </div>
-    );
+    const { pathname } = useRouter();
+
+    const containerClasses = classNames({
+        "w-full h-full": 1,
+        "px-[6%] min-[550px]:px-[8%] min-[750px]:px-[12%]": pathname.includes("/explore"),
+        "min-[750px]:px-[6%] min-[1300px]:px-[12%] 1": pathname.includes("/profile"),
+    });
+
+    return <div className={containerClasses}>{children}</div>;
 };
 
 export default PagePadding;
