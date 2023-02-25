d9c734763250835b0789d2ee43e8c8de39be158c
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-25 23:23:06 +0330

    Update form-input component

--- pages/auth/components/form-input/index.tsx
+++ pages/auth/components/form-input/index.tsx
@@ -1,4 +1,4 @@
-import classNames from "classnames";
+import Input from "components/input";
 import { ErrorMessage, useField } from "formik";
 import React, { useRef } from "react";
 import { TFormInputProps } from "./form-input.types";
@@ -9,33 +9,18 @@ const FormInput: React.FC<TFormInputProps> = ({ rightElement: RightElement, ...p
 
     const focusOnInput = () => inputRef.current?.focus();
 
-    const inputContainerClasses = classNames({
-        "w-full group px-4 h-[50px] flex justify-between items-center border-[2px] transition-colors duration-300 rounded-[18px] border-2 border-[rgb(85,85,85)] hover:border-[rgb(180,180,180)] focus-within:border-[rgb(180,180,180)] ": 1,
-        "border-red-500": !!meta.error && meta.touched,
-    });
-
-    const inputClasses = classNames({
-        "bg-transparent outline-none w-full text-[18px] placeholder:text-[#7a7a7a] text-[#ffffff]": 1,
-    });
-
     return (
-        <div
-            className="w-full flex flex-col justify-start items-start cursor-text"
-            onClick={focusOnInput}
-        >
-            <div className={inputContainerClasses}>
-                {/* @ts-ignore */}
-                <input
-                    {...field}
-                    {...props}
-                    ref={inputRef}
-                    className={inputClasses}
-                    autoComplete="off"
-                />
-                {RightElement && (
-                    <RightElement className="fill-[rgb(180,180,180)] w-[20px]" />
-                )}
-            </div>
+        <div className="w-full flex flex-col justify-start items-start cursor-text" onClick={focusOnInput}>
+            {/* @ts-ignore */}
+            <Input
+                {...field}
+                {...props}
+                error={!!meta.error && meta.touched}
+                ref={inputRef}
+                icon={RightElement}
+                autoComplete="off"
+                fullWidth
+            />
             <ErrorMessage component="div" name={props.name}>
                 {(errMsg) => (
                     <div className="text-red-400 text-[16px] font-medium flex justify-start items-center gap-2 px-3 sm:px-4 mt-2">
