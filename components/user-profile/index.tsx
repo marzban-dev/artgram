5b9f0f02bb6a7fa07234b54bab3a25f8a0d097eb
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-25 23:22:23 +0330

    Create user-profile component

---
+++ components/user-profile/index.tsx
@@ -0,0 +1,28 @@
+import Avatar from "components/avatar";
+import { useNotificationsQuery } from "hooks/use-notifications";
+import { useSession } from "next-auth/react";
+import { useMemo } from "react";
+
+const UserProfile: React.FC = () => {
+    const { data } = useSession();
+    const { data: notifications } = useNotificationsQuery({ enabled: true });
+
+    const isThereNewNotification = useMemo(() => {
+        if (notifications) {
+            const lastNotifications = notifications.pages.at(-1)!;
+            return notifications && lastNotifications.count !== 0 && lastNotifications.items[0].is_read === false;
+        }
+
+        return false;
+    }, [notifications]);
+
+    return (
+        <div className="relative flex justify-center items-center">
+            {isThereNewNotification && (
+                <div className="absolute right-[-14px] w-[6px] h-[6px] rounded-full bg-red-500 z-10" />
+            )}
+            <Avatar picture={data!.user.profile_img} title={data!.user.username} width={28} height={28} />
+        </div>
+    );
+};
+export default UserProfile;
