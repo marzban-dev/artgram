dc6e807264dcc0cec1b8e80b1142c5f1d69aca54
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-25 23:24:00 +0330

    Update navbar component

--- pages/explore/components/navbar/index.tsx
+++ pages/explore/components/navbar/index.tsx
@@ -1,23 +1,17 @@
+import Logo from "components/logo";
+import AppNavbar from "components/navbar";
 import PagePadding from "layouts/page-padding";
-import Image from "next/image";
-import Logo from "public/assets/img/logo.jpg";
 
 const Navbar: React.FC = () => {
     return (
-        <nav className="w-full h-[80px]">
+        <div className="w-full h-[80px]">
             <PagePadding>
                 <div className="w-full h-full flex justify-between items-center gap-8">
-                    <div className="w-[40px]">
-                        <Image src={Logo} alt="logo" />
-                    </div>
-                    <ul className="flex justify-start items-center gap-8">
-                        <li className="text-white">Explore</li>
-                        <li className="text-white">Request Artist</li>
-                        <li className="text-white">About</li>
-                    </ul>
+                    <Logo />
+                    <AppNavbar />
                 </div>
             </PagePadding>
-        </nav>
+        </div>
     );
 };
 
