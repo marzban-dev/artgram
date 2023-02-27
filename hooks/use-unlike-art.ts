3fcd6cbdf9ca27ea014c9d272365d10d54343bd3
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-27 15:27:23 +0330

    Update use-unlike-art hook

--- hooks/use-unlike-art.ts
+++ hooks/use-unlike-art.ts
@@ -1,7 +1,10 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InfiniteData, useMutation, useQueryClient } from "@tanstack/react-query";
 import { unlikeArt } from "api/arts.api";
+import { IArtLike } from "api/arts.types";
+import { useSession } from "next-auth/react";
 
 export const useUnlikeArtMutation = (id: number) => {
+    const { data: authedUser } = useSession();
     const queryClient = useQueryClient();
     const key = ["art-likes", id];
 
@@ -11,15 +14,35 @@ export const useUnlikeArtMutation = (id: number) => {
             await queryClient.cancelQueries();
             const prevQueryData = queryClient.getQueryData(key);
 
-            queryClient.setQueryData<{ likesCount: number; users: any[] }>(key, (prevQueryData) => {
-                if (prevQueryData !== undefined) {
-                    return {
-                        likesCount: prevQueryData.likesCount - 1,
-                        users: prevQueryData.users,
-                    };
+            queryClient.setQueryData<InfiniteData<{ count: number; next: string | null; items: IArtLike[] }>>(
+                key,
+                (prevQueryData) => {
+                    if (prevQueryData !== undefined) {
+                        const newPagesArray = prevQueryData.pages.map((page) => {
+                            const artLikeIndex = page.items.findIndex(
+                                (artLike) => artLike.owner.username === authedUser?.user.username
+                            );
+
+                            if (artLikeIndex) {
+                                const copyOfItems = page.items;
+                                copyOfItems.splice(artLikeIndex, 1);
+
+                                return {
+                                    items: copyOfItems,
+                                    count: page.count - 1,
+                                    next: page.next,
+                                };
+                            } else return page;
+                        });
+
+                        return {
+                            pages: newPagesArray,
+                            pageParams: prevQueryData.pageParams,
+                        };
+                    }
+                    return prevQueryData;
                 }
-                return prevQueryData;
-            });
+            );
 
             return { prevQueryData };
         },
