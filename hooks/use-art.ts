e062584730dd3845dab8cbd282c29382040a19a5
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-27 15:26:25 +0330

    Update use-art hook

--- hooks/use-art.ts
+++ hooks/use-art.ts
@@ -1,17 +1,6 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import { getArt } from "api/arts.api";
 
 export const useArtQuery = (id: number) => {
-    const queryClient = useQueryClient();
-
-    return useQuery(["art", id], () => getArt({ id }), {
-        initialData: () => {
-            // const cachedArts = queryClient.getQueryData<IArt[]>(["explore"]);
-
-            // const art = cachedArts?.find((art) => art.id === id);
-
-            // if (art) return art;
-            return undefined;
-        },
-    });
+    return useQuery(["art", id], () => getArt({ id }));
 };
