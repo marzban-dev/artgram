ab13a4d8e86dcd447fe2ec47ef447094db265f11
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-27 15:27:42 +0330

    Update page-loading layout

--- layouts/page-loading/index.tsx
+++ layouts/page-loading/index.tsx
@@ -2,7 +2,7 @@ import Spinner from "components/spinner";
 
 const PageLoading: React.FC = () => {
     return (
-        <div className="w-full h-screen bg-black flex justify-center items-center">
+        <div className="w-full h-[100dvh] bg-black flex justify-center items-center">
             <Spinner size={45} />
         </div>
     );
