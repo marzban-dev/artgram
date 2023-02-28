ff0a262ee34052244518301e3b275e9e99215c0f
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-28 22:32:59 +0330

    Update art page

--- pages/art/[id].page.tsx
+++ pages/art/[id].page.tsx
@@ -4,12 +4,8 @@ import { useArtQuery } from "hooks/use-art";
 import useHideOverflow from "hooks/use-hide-overflow";
 import PageTransition from "layouts/page-transition";
 import { GetServerSideProps, NextPage } from "next";
-import { getSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { RootState } from "store";
-import { setOrderBy, setSearchBy, TOrderBy, TSearchBy } from "store/slice/explore.slice";
 import { TArtPageUrlParams } from "./art.types";
 import Arts from "./components/arts";
 import Background from "./components/background";
@@ -22,7 +18,10 @@ const ArtPage: NextPage = () => {
     useHideOverflow();
 
     useEffect(() => {
-        setWindowHeight(window.innerHeight);
+        const divElement = document.querySelector("#full-height-element") as HTMLDivElement;
+        if (window.innerWidth < 520) {
+            setWindowHeight(divElement.clientHeight - 50);
+        } else setWindowHeight(divElement.clientHeight);
     }, []);
 
     return (
@@ -33,7 +32,7 @@ const ArtPage: NextPage = () => {
                 <Fullscreen />
 
                 {windowHeight !== 0 && (
-                    <div className="relative z-[40]">
+                    <div className="relative z-[40] bg-black min-[520px]:bg-transparent">
                         <Arts id={Number(query.id)} art={art!} containerHeight={windowHeight} />
                     </div>
                 )}
@@ -43,54 +42,30 @@ const ArtPage: NextPage = () => {
 };
 
 export const getServerSideProps: GetServerSideProps<any, TArtPageUrlParams> = async (context) => {
-    const session = await getSession({ req: context.req });
     const queryClient = new QueryClient();
 
     const artId = Number(context.params!.id);
 
-    try {
-        const art = await getArt({ id: artId, token: session?.accessToken });
-        await queryClient.prefetchQuery(["art", artId], () => {
-            return art;
-            // return {
-            //     id: 7306,
-            //     artist: {
-            //         id: 909,
-            //         name: "CAPORALI, Bartolomeo",
-            //         image: "https://www.wga.hu/biojpg/c/caporali/biograph.jpg",
-            //         following: false,
-            //     },
-            //     title: "Annunciation",
-            //     date: "1467-68",
-            //     technique: "Tempera, oil and gold on panel, 50 x 48 cm (each)",
-            //     location: "Galleria Nazionale dell'Umbria, Perugia",
-            //     image: {
-            //         id: 7306,
-            //         url: "https://www.wga.hu/art/c/caporali/annuncia.jpg",
-            //         width: 1629,
-            //         height: 800,
-            //         thumbnail: "https://www.wga.hu/preview/c/caporali/annuncia.jpg",
-            //     },
-            //     form: "painting",
-            //     school: "Italian",
-            //     type: "religious",
-            //     reference: "www.wga.hu",
-            //     likes_count: 1,
-            //     user_like: true,
-            //     user_repost: false,
-            // };
-        });
-    } catch (e) {
-        return {
-            notFound: true,
-        };
+    const isRequestFromRouter = context.req.url?.includes("_next");
+
+    if (!isRequestFromRouter) {
+        try {
+            const art = await getArt({ id: artId });
+            await queryClient.prefetchQuery(["art", artId], () => art);
+
+            return {
+                props: {
+                    dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+                },
+            };
+        } catch (e) {
+            return {
+                notFound: true,
+            };
+        }
     }
 
-    return {
-        props: {
-            dehydratedState: dehydrate(queryClient),
-        },
-    };
+    return { props: {} };
 };
 
 export default ArtPage;
