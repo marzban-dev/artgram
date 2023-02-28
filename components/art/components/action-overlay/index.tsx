e61e46283ca4652d96ca9c3e669451c0d963114a
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-28 22:28:41 +0330

    Update action-overlay component

--- components/art/components/action-overlay/index.tsx
+++ components/art/components/action-overlay/index.tsx
@@ -1,23 +1,21 @@
-import { Fragment } from "react";
-import { motion, Variants } from "framer-motion";
 import Like from "components/like";
+import { motion, Variants } from "framer-motion";
+import { Fragment } from "react";
+import { useMediaQuery } from "react-responsive";
 import { IActionOverlayProps } from "./action-overlay.types";
 
 const ActionOverlay: React.FC<IActionOverlayProps> = ({ showActions, id, user_like, setIsLiked }) => {
+    const isMobile = useMediaQuery({ maxWidth: 400 });
+
     const actionsVariants: Variants = {
         hide: {
             opacity: 0,
-            scale: 0.9,
             transition: {
-                duration: 0.3,
-                opacity: {
-                    duration: 0.2,
-                },
+                duration: 0.2,
             },
         },
         show: {
             opacity: 1,
-            scale: 1,
             transition: {
                 duration: 0.2,
             },
@@ -34,11 +32,12 @@ const ActionOverlay: React.FC<IActionOverlayProps> = ({ showActions, id, user_li
             <motion.div
                 variants={actionsVariants}
                 animate={showActions ? "show" : "hide"}
-                className="absolute bottom-[10px] right-[20px]"
+                className="absolute bottom-[2px] min-[420px]:bottom-[10px] right-[10px] min-[420px]:right-[20px]"
             >
                 <Like
                     id={id}
                     initial={user_like}
+                    size={isMobile ? 22 : 25}
                     onLikeCallback={() => setIsLiked(true)}
                     onDislikeCallback={() => setIsLiked(false)}
                 />
