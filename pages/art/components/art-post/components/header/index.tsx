12ebbe3d72151b28ee481b85d0981cf5c3d25c8e
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-28 22:31:37 +0330

    Update header component

--- pages/art/components/art-post/components/header/index.tsx
+++ pages/art/components/art-post/components/header/index.tsx
@@ -4,10 +4,10 @@ import { IHeaderProps } from "./header.types";
 const Header: React.FC<IHeaderProps> = ({ id, title, artist, year }) => {
     return (
         <div className="flex justify-start items-start flex-col max-w-[calc(100%_-_100px)]">
-            <h2 className="text-white text-[20px] overflow-hidden whitespace-nowrap text-ellipsis w-full">
+            <h2 className="text-white text-[16px] min-[800px]:text-[20px] overflow-hidden whitespace-nowrap text-ellipsis w-full">
                 {title}
             </h2>
-            <div className="text-[rgb(150,150,150)] text-[15px] overflow-hidden whitespace-nowrap text-ellipsis w-full">
+            <div className="text-[rgb(150,150,150)] text-[12px] min-[800px]:text-[15px] overflow-hidden whitespace-nowrap text-ellipsis w-full">
                 <Link href={`/profile/artist/${id}`}>{artist}</Link> - <span>{year}</span>
             </div>
         </div>
