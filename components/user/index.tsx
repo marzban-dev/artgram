da1ca24c19ef31c0784f36067e04cc30df201b68
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-02-28 22:29:49 +0330

    Update user component

--- components/user/index.tsx
+++ components/user/index.tsx
@@ -4,8 +4,8 @@ import { IUserProps } from "./user.types";
 
 const User: React.FC<IUserProps> = ({ avatar, firstName, username, isFollowing }) => {
     return (
-        <div className="w-full flex justify-start items-center gap-4 h-[60px]">
-            <Avatar width={60} height={60} picture={avatar} title={username} />
+        <div className="w-full flex justify-start items-center gap-4 h-[60px] ">
+            <Avatar picture={avatar} title={username} />
             <div className="h-full w-[calc(100%-190px)] flex flex-col justify-center items-start text-white">
                 <span className="text-[18px]">{firstName}</span>
                 <span className="text-[rgb(150,150,150)] text-[16px]">{username}</span>
