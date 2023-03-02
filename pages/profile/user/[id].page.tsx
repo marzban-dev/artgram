c397ed4b857cc96a6346d2ebbfce861272a1f724
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-02 16:36:06 +0330

    Update user page

--- pages/profile/user/[id].page.tsx
+++ pages/profile/user/[id].page.tsx
@@ -1,17 +1,18 @@
 import { dehydrate, QueryClient } from "@tanstack/react-query";
 import { getUserProfile } from "api/user.api";
 import { useUserQuery } from "hooks/use-user";
-import { GetServerSideProps, NextPage } from "next";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
 import { useRouter } from "next/router";
 import CalenderIcon from "public/assets/icon/calendar-star.svg";
 import LinkIcon from "public/assets/icon/link.svg";
 import LocationIcon from "public/assets/icon/location-dot.svg";
 import { useMemo } from "react";
 import ProfileContainer from "../components/profile-container";
+import { TUserPageProps } from "./user.types";
 
 const UserPage: NextPage = () => {
     const { query } = useRouter();
-    const { data: userData } = useUserQuery(String(query.id));
+    const { data: userData, isFetching } = useUserQuery(String(query.id));
 
     const profileInfo = useMemo(() => {
         if (userData) {
@@ -35,36 +36,39 @@ const UserPage: NextPage = () => {
             profileInfo={profileInfo}
             followers={userData.followers_count}
             following={userData.followings_count}
+            isFetching={isFetching}
             type="user"
         />
     ) : null;
 };
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        fallback: "blocking",
+        paths: [],
+    };
+};
+
+export const getStaticProps: GetStaticProps<any, TUserPageProps> = async (context) => {
     const queryClient = new QueryClient();
 
     const userId = String(context.params!.id);
 
-    const isRequestFromRouter = context.req.url?.includes("_next");
+    try {
+        const userProfile = await getUserProfile({ id: userId });
+        await queryClient.prefetchQuery(["user", userId], () => userProfile);
 
-    if (!isRequestFromRouter) {
-        try {
-            const userProfile = await getUserProfile({ id: userId });
-            await queryClient.prefetchQuery(["user", userId], () => userProfile);
-
-            return {
-                props: {
-                    dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
-                },
-            };
-        } catch (e) {
-            return {
-                notFound: true,
-            };
-        }
+        return {
+            revalidate: 60,
+            props: {
+                dehydratedState: JSON.parse(JSON.stringify(dehydrate(queryClient))),
+            },
+        };
+    } catch (e) {
+        return {
+            notFound: true,
+        };
     }
-
-    return { props: {} };
 };
 
 export default UserPage;
