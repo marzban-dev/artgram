10b1df58189597f6f9e43014c07f780b922f64f4
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-03 16:52:03 +0330

    Update use-following hook

--- hooks/use-following.ts
+++ hooks/use-following.ts
@@ -1,12 +1,13 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { getFollowing } from "api/user.api";
 
-export const useFollowingQuery = (id: string, type: "user" | "artist", initialData: number = 0) => {
+export const useFollowingQuery = (id: string, type: "user" | "artist", enabled: boolean) => {
     const pageParamDefaults = { id, type, limit: 15, page: 1 };
 
     return useInfiniteQuery(["following", id], ({ pageParam = pageParamDefaults }) => getFollowing({ pageParam }), {
+        enabled,
         placeholderData: {
-            pages: [{ items: [], next: "", count: initialData }],
+            pages: [{ items: [], next: "", count: 0 }],
             pageParams: [null],
         },
         getNextPageParam: (lastPage, pages) => {
