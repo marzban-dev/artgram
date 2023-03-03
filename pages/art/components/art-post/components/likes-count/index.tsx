bfe06ab2c63dcb49bb284aaf4423433209c449ff
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-03 16:52:35 +0330

    Update likes-count component

--- pages/art/components/art-post/components/likes-count/index.tsx
+++ pages/art/components/art-post/components/likes-count/index.tsx
@@ -1,14 +1,14 @@
 import Likes from "components/likes";
-import { useArtLikesQuery } from "hooks/use-art-likes";
-import React, { Fragment, useState } from "react";
+import { useInView } from "framer-motion";
+import { useCountQuery } from "hooks/use-count";
+import React, { Fragment, useRef, useState } from "react";
 import { ILikesCountProps } from "./likes-count.types";
 
 const LikesCount: React.FC<ILikesCountProps> = ({ id, initial }) => {
     const [show, setShow] = useState(false);
-
-    const { data } = useArtLikesQuery(id, initial);
-
-    const count = data!.pages.at(-1)!.count;
+    const containerRef = useRef<HTMLDivElement>(null);
+    const isInView = useInView(containerRef);
+    const { data: count } = useCountQuery(["art-likes", id], initial, { id }, isInView);
 
     const containerStyle: React.CSSProperties = {
         opacity: count !== 0 ? 1 : 0,
@@ -18,13 +18,14 @@ const LikesCount: React.FC<ILikesCountProps> = ({ id, initial }) => {
         <Fragment>
             <div
                 className="text-[16px] text-[rgb(150,150,150)] font-semibold cursor-pointer"
-                style={containerStyle}
                 onClick={() => setShow(true)}
+                style={containerStyle}
+                ref={containerRef}
             >
                 <span className="mr-[6px] text-white">{count}</span>
                 <span>Likes</span>
             </div>
-            <Likes id={id} initialData={initial} show={show} setShow={setShow} />
+            <Likes id={id} show={show} setShow={setShow} />
         </Fragment>
     );
 };
