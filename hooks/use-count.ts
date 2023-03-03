51dad6b6e5ce6eb649a6dd8cb08ad25b1adf9170
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-03 16:51:44 +0330

    Create use-count hook

---
+++ hooks/use-count.ts
@@ -0,0 +1,37 @@
+import { QueryKey, useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { getArtLikes } from "api/arts.api";
+import { getFollowers, getFollowing, getNotifications } from "api/user.api";
+
+type TRequestTypes = "following" | "followers" | "notifications" | "art-likes";
+
+type TRequestParams = {
+    id?: string | number;
+    type?: "user" | "artist";
+};
+
+type TKey = [type: TRequestTypes, id: number | string];
+
+export const useCountQuery = (key: TKey, initial: number, params?: TRequestParams, enabled: boolean = true) => {
+    const pageParamDefaults = { limit: 0, page: 1 };
+
+    return useQuery(
+        [key[0] + "-count", key[1]],
+        async ({ pageParam = pageParamDefaults }) => {
+            const fetchData = async () => {
+                if (key[0] === "notifications") return getNotifications({ pageParam });
+                if (key[0] === "followers")
+                    return getFollowers({ pageParam: { ...pageParam, id: params!.id, type: params!.type } });
+                if (key[0] === "following")
+                    return getFollowing({ pageParam: { ...pageParam, id: params!.id, type: params!.type } });
+                if (key[0] === "art-likes") return getArtLikes({ pageParam: { ...pageParam, id: params!.id } });
+            };
+
+            const response = await fetchData();
+            return response!.count;
+        },
+        {
+            enabled,
+            placeholderData: initial,
+        }
+    );
+};
