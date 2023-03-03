59890bba844fc8dfabfdf5d3105ba2344e9dac12
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-03 16:51:55 +0330

    Update use-followers hook

--- hooks/use-followers.ts
+++ hooks/use-followers.ts
@@ -1,15 +1,16 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { getFollowers } from "api/user.api";
 
-export const useFollowersQuery = (id: string | number, type: "user" | "artist", initialData: number = 0) => {
+export const useFollowersQuery = (id: string | number, type: "user" | "artist", enabled: boolean) => {
     const pageParamDefaults = { id, type, limit: 15, page: 1 };
 
     return useInfiniteQuery(
         ["followers", type === "artist" ? Number(id) : id],
         ({ pageParam = pageParamDefaults }) => getFollowers({ pageParam }),
         {
+            enabled,
             placeholderData: {
-                pages: [{ items: [], next: "", count: initialData }],
+                pages: [{ items: [], next: "", count: 0 }],
                 pageParams: [null],
             },
             getNextPageParam: (lastPage, pages) => {
