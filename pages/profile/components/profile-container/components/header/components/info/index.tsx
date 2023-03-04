386c4917c7f05f3114def2bd071981faaccb2b2e
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-05 02:08:19 +0330

    Update info component

--- pages/profile/components/profile-container/components/header/components/info/index.tsx
+++ pages/profile/components/profile-container/components/header/components/info/index.tsx
@@ -1,18 +1,21 @@
 import { Fragment, memo } from "react";
 import Followers from "./components/followers";
 import Following from "./components/following";
+import FollowingArtists from "./components/following-artists";
 import { IInfoProps } from "./info.types";
 
-const Info: React.FC<IInfoProps> = ({ id, followers, following, username, type }) => {
+const Info: React.FC<IInfoProps> = ({ id, followers, following, followingArtists, username, type }) => {
     return (
         <div className="flex justify-start items-center gap-2">
             <span className="text-[rgb(160,160,160)] font-semibold text-[14px] whitespace-nowrap">@{username}</span>
             <span className="w-[4px] h-[4px] rounded-full bg-profile-primary" />
             <Followers id={id} type={type} initial={followers} />
-            {following !== undefined && (
+            {following !== undefined && followingArtists !== undefined && (
                 <Fragment>
                     <span className="w-[4px] h-[4px] rounded-full bg-profile-primary" />
                     <Following id={id} type={type} initial={following} />
+                    <span className="w-[4px] h-[4px] rounded-full bg-profile-primary" />
+                    <FollowingArtists id={id} initial={followingArtists} />
                 </Fragment>
             )}
         </div>
--- pages/profile/components/profile-container/components/header/components/info/info.types.ts
+++ pages/profile/components/profile-container/components/header/components/info/info.types.ts
@@ -2,6 +2,7 @@ export interface IInfoProps {
     id: string;
     followers: number;
     following?: number;
+    followingArtists?: number;
     username: string;
     type: "user" | "artist";
 }
