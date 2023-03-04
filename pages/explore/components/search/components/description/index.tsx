c2e75381a2634fadc8d1fa779e2938e9cfee863c
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-05 02:06:42 +0330

    Update description component

--- pages/explore/components/search/components/description/index.tsx
+++ pages/explore/components/search/components/description/index.tsx
@@ -1,10 +1,10 @@
 const Description: React.FC = () => {
     return (
         <div className="z-20 flex justify-center items-center flex-col">
-            <h1 className="text-white text-[40px] font-bold w-[500px] text-center [line-height:1.2]">
+            <h1 className="text-white text-[25px] min-[600px]:text-[30px] min-[850px]:text-[40px] font-bold w-[350px] min-[600px]:w-[400px] min-[850px]:w-[500px] text-center [line-height:1.2]">
                 Explore the world`s leading design portfolios
             </h1>
-            <h2 className="text-[rgb(210,210,210)] text-[18px] text-center mt-5 mb-10 w-[700px]">
+            <h2 className="text-[rgb(210,210,210)] text-[14px] min-[600px]:text-[16px] min-[850px]:text-[18px] text-center mt-5 mb-10 w-[350px] min-[600px]:w-[450px] min-[850px]:w-[700px]">
                 Millions of designers and agencies around the world showcase their portfolio work on Dribbble - the home
                 to the world`s best design and creative professionals.
             </h2>
