2812f33df536dc6add4b8a1c8f20c5617e68cdde
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-05 02:07:01 +0330

    Update search-input component

--- pages/explore/components/search/components/search-input/index.tsx
+++ pages/explore/components/search/components/search-input/index.tsx
@@ -5,7 +5,7 @@ import Input from "./components/input";
 const SearchInput: React.FC = () => {
     return (
         <Fragment>
-            <div className="w-[450px] h-[55px] bg-[rgb(25,25,25)] border-2 border-[rgb(40,40,40)] rounded-[30px] px-5 z-20 flex justify-start items-center focus-within:border-[rgb(60,60,60)] transition-colors">
+            <div className="w-[330px] min-[600px]:w-[450px] h-[55px] bg-[rgb(25,25,25)] border-2 border-[rgb(40,40,40)] rounded-[30px] px-5 z-20 flex justify-start items-center focus-within:border-[rgb(60,60,60)] transition-colors">
                 <Input />
                 <FilterButton />
             </div>
