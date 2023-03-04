b1dd7b3475aebc51a8ab9e500904aefd5da3c21c
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-05 02:02:54 +0330

    Create use-following-artists hook

---
+++ hooks/use-following-artists.ts
@@ -0,0 +1,26 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { getFollowingArtist } from "api/user.api";
+
+export const useFollowingArtistsQuery = (id: string, enabled: boolean) => {
+    const pageParamDefaults = { id, type: "artist", limit: 15, page: 1 };
+
+    return useInfiniteQuery(
+        ["following-artists", id],
+        ({ pageParam = pageParamDefaults }) => getFollowingArtist({ pageParam }),
+        {
+            enabled,
+            placeholderData: {
+                pages: [{ items: [], next: "", count: 0 }],
+                pageParams: [null],
+            },
+            getNextPageParam: (lastPage, pages) => {
+                const nextPage = {
+                    ...pageParamDefaults,
+                    page: pages.length + 1,
+                };
+
+                return lastPage.next ? nextPage : undefined;
+            },
+        }
+    );
+};
