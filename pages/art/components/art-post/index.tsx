b25eb096c332eccdc5ad0b7909b45fb3f149e55c
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-05 02:05:28 +0330

    Update art-post component

--- pages/art/components/art-post/index.tsx
+++ pages/art/components/art-post/index.tsx
@@ -2,8 +2,6 @@ import Avatar from "components/avatar";
 import FollowButton from "components/follow-button";
 import Like from "components/like";
 import useImageColors from "hooks/use-image-colors";
-import LocationIcon from "public/assets/icon/location-dot.svg";
-import BrushIcon from "public/assets/icon/paintbrush.svg";
 import { useEffect, useRef } from "react";
 import setCssVariables from "utils/set-css-variables";
 import { IArtPostProps } from "./art-post.types";
@@ -48,7 +46,7 @@ const ArtPost: React.FC<IArtPostProps> = ({
     }, [imageColors]);
 
     return (
-        <section className="w-full flex justify-center items-center min-[520px]:mt-[30px] snap-center last:mb-[30px]">
+        <section className="w-full flex justify-center items-center min-[520px]:mt-[30px] snap-none last:mb-[30px]">
             <div
                 className="w-full min-[520px]:max-w-[450px] min-[800px]:max-w-[550px] overflow-hidden py-4 bg-black min-[520px]:bg-[rgb(20,20,20)] min-[520px]:rounded-2xl"
                 id={`art-container-${id}`}
@@ -74,10 +72,14 @@ const ArtPost: React.FC<IArtPostProps> = ({
                         height={image.height}
                         priority={priority}
                     />
-                    <div className="flex justify-center items-start flex-col absolute z-20 bottom-[16px] left-[18px] gap-2">
-                        <ArtDetail id={id} icon={BrushIcon} text={type} iconSize={14} />
-                        <ArtDetail id={id} icon={LocationIcon} text={location} iconSize={10} />
-                    </div>
+                    <ArtDetail
+                        id={id}
+                        location={location}
+                        type={type}
+                        form={form}
+                        school={school}
+                        technique={technique}
+                    />
                 </div>
                 <div className="flex justify-between items-center px-5 pt-4">
                     <div className="flex justify-start items-center gap-3">
