d05d30db8af05b63c2d94b0b908af2c8314369c5
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-05 02:03:28 +0330

    Update page-padding layout

--- layouts/page-padding/index.tsx
+++ layouts/page-padding/index.tsx
@@ -7,7 +7,7 @@ const PagePadding: React.FC<IPagePaddingProps> = ({ children }) => {
 
     const containerClasses = classNames({
         "w-full h-full": 1,
-        "px-[6%] min-[550px]:px-[8%] min-[750px]:px-[12%]": pathname.includes("/explore"),
+        "px-[2%] min-[550px]:px-[8%] min-[750px]:px-[12%]": pathname.includes("/explore"),
         "min-[750px]:px-[6%] min-[1300px]:px-[12%]": pathname.includes("/profile"),
     });
 
