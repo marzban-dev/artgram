44731b0aca79b506474a12da250b4c3dc2018245
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-05 02:03:17 +0330

    Update use-unlike-art hook

--- hooks/use-unlike-art.ts
+++ hooks/use-unlike-art.ts
@@ -1,12 +1,9 @@
-import { InfiniteData, useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { unlikeArt } from "api/arts.api";
-import { IArtLike } from "api/arts.types";
-import { useSession } from "next-auth/react";
 
 export const useUnlikeArtMutation = (id: number) => {
-    const { data: authedUser } = useSession();
     const queryClient = useQueryClient();
-    const key = ["art-likes", id];
+    const key = ["art-likes-count", id];
 
     return useMutation({
         mutationFn: unlikeArt,
@@ -14,35 +11,10 @@ export const useUnlikeArtMutation = (id: number) => {
             await queryClient.cancelQueries();
             const prevQueryData = queryClient.getQueryData(key);
 
-            queryClient.setQueryData<InfiniteData<{ count: number; next: string | null; items: IArtLike[] }>>(
-                key,
-                (prevQueryData) => {
-                    if (prevQueryData !== undefined) {
-                        const newPagesArray = prevQueryData.pages.map((page) => {
-                            const artLikeIndex = page.items.findIndex(
-                                (artLike) => artLike.owner.username === authedUser?.user.username
-                            );
-
-                            if (artLikeIndex) {
-                                const copyOfItems = page.items;
-                                copyOfItems.splice(artLikeIndex, 1);
-
-                                return {
-                                    items: copyOfItems,
-                                    count: page.count - 1,
-                                    next: page.next,
-                                };
-                            } else return page;
-                        });
-
-                        return {
-                            pages: newPagesArray,
-                            pageParams: prevQueryData.pageParams,
-                        };
-                    }
-                    return prevQueryData;
-                }
-            );
+            queryClient.setQueryData<number>(key, (prevQueryData) => {
+                if (prevQueryData !== undefined) return prevQueryData - 1;
+                return prevQueryData;
+            });
 
             return { prevQueryData };
         },
