a780f9de7431304d0134bbb8a0074a0460675142
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-05 15:38:34 +0330

    Update user component

--- components/user/index.tsx
+++ components/user/index.tsx
@@ -1,15 +1,20 @@
 import Avatar from "components/avatar";
 import FollowButton from "components/follow-button";
+import Link from "next/link";
 import { IUserProps } from "./user.types";
 
 const User: React.FC<IUserProps> = ({ id, avatar, title, type, underTitle, isFollowing }) => {
+    const href = `/profile/${type}/${id}`;
+
     return (
-        <div className="w-full flex justify-start items-center gap-4 h-[60px] ">
-            <Avatar picture={avatar} title={title} />
-            <div className="h-full w-[calc(100%-190px)] flex flex-col justify-center items-start text-white">
-                <span className="text-[18px]">{underTitle}</span>
-                <span className="text-[rgb(150,150,150)] text-[16px]">{title}</span>
-            </div>
+        <div className="w-full flex justify-start items-center gap-4 h-[60px]">
+            <Link href={href}>
+                <Avatar picture={avatar} title={title} />
+            </Link>
+            <Link href={href} className="h-full w-[calc(100%-190px)] flex flex-col justify-center items-start text-white">
+                <span className="text-[18px]">{title}</span>
+                <span className="text-[rgb(150,150,150)] text-[16px]">{underTitle}</span>
+            </Link>
             <FollowButton id={id} type={type} width={100} colorClass="profile" initial={isFollowing} />
         </div>
     );
