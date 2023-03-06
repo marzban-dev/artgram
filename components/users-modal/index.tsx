9049ca032078c22668255db4d30262b1728dd17c
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-06 17:39:28 +0330

    Update user-modal component

--- components/users-modal/index.tsx
+++ components/users-modal/index.tsx
@@ -6,24 +6,19 @@ import { IUsersModalProps } from "./users-modal.types";
 
 const UsersModal: React.FC<IUsersModalProps> = ({ show, setShow, title, users, fetchNextPage, hasNextPage }) => {
     return (
-        <Modal show={show} onClose={() => setShow(false)}>
-            <div className="pt-2 pb-5 min-[520px]:py-5 px-6 overflow-y-auto max-h-[500px] scrollbar-custom relative">
-                <div className="flex justify-center items-center border-b-2 border-[rgb(40,40,40)] pb-2 mb-6">
-                    <span className="text-white font-medium text-[25px]">{title}</span>
-                </div>
-                {users.length === 0 ? (
-                    <UserPlaceholder />
-                ) : (
-                    <InfiniteScroll
-                        dataLength={users.length}
-                        next={fetchNextPage}
-                        hasMore={!!hasNextPage}
-                        loader={<Spinner size={40} style={{ padding: "50px 0" }} />}
-                    >
-                        <div className="flex justify-start items-center flex-col gap-5 max-[520px]:pb-8">{users}</div>
-                    </InfiniteScroll>
-                )}
-            </div>
+        <Modal title={title} show={show} onClose={() => setShow(false)}>
+            {users.length === 0 ? (
+                <UserPlaceholder />
+            ) : (
+                <InfiniteScroll
+                    dataLength={users.length}
+                    next={fetchNextPage}
+                    hasMore={!!hasNextPage}
+                    loader={<Spinner size={40} style={{ padding: "50px 0" }} />}
+                >
+                    <div className="flex justify-start items-center flex-col gap-5 max-[520px]:pb-8">{users}</div>
+                </InfiniteScroll>
+            )}
         </Modal>
     );
 };
