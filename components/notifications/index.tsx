005490ff5939500348a74968317a7f0878aad969
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-06 17:39:08 +0330

    Update notifications component

--- components/notifications/index.tsx
+++ components/notifications/index.tsx
@@ -17,7 +17,7 @@ const Notifications: React.FC<INotificationsProps> = ({ show, setShow }) => {
 
     useEffect(() => {
         if (notifications) seenNotifications({ id: flatInfiniteQueryData<INotification>(notifications)[0].id });
-    }, [notifications]);
+    }, [notifications, seenNotifications]);
 
     const renderNotifications = useMemo(() => {
         if (notifications) {
@@ -37,29 +37,24 @@ const Notifications: React.FC<INotificationsProps> = ({ show, setShow }) => {
     }, [notifications]);
 
     return (
-        <Modal show={show} onClose={() => setShow(false)}>
-            <div className="pt-2 pb-5 min-[520px]:py-5 px-6 overflow-y-scroll max-h-[500px] scrollbar-custom relative">
-                <div className="flex justify-center items-center border-b-2 border-[rgb(40,40,40)] pb-2 mb-6">
-                    <span className="text-white font-medium text-[25px]">Notifications</span>
-                </div>
-                {!renderNotifications ? (
-                    <NotificationPlaceholder />
-                ) : (
-                    <InfiniteScroll
-                        dataLength={renderNotifications.length}
-                        next={fetchNextPage}
-                        hasMore={!!hasNextPage}
-                        loader={<Spinner size={40} style={{ padding: "50px 0" }} />}
+        <Modal title="Notifications" show={show} onClose={() => setShow(false)}>
+            {!renderNotifications ? (
+                <NotificationPlaceholder />
+            ) : (
+                <InfiniteScroll
+                    dataLength={renderNotifications.length}
+                    next={fetchNextPage}
+                    hasMore={!!hasNextPage}
+                    loader={<Spinner size={40} style={{ padding: "50px 0" }} />}
+                >
+                    <div
+                        className="flex justify-start items-center flex-col gap-5 max-[520px]:pb-8"
+                        ref={notificationsContainer}
                     >
-                        <div
-                            className="flex justify-start items-center flex-col gap-5 max-[520px]:pb-8"
-                            ref={notificationsContainer}
-                        >
-                            {renderNotifications}
-                        </div>
-                    </InfiniteScroll>
-                )}
-            </div>
+                        {renderNotifications}
+                    </div>
+                </InfiniteScroll>
+            )}
         </Modal>
     );
 };
