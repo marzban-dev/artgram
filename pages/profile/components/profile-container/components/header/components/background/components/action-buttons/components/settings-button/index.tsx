7c0d19def6374a77ece2bd0b85dd21b225b89d48
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-06 17:44:37 +0330

    Update settings-button component

--- pages/profile/components/profile-container/components/header/components/background/components/action-buttons/components/settings-button/index.tsx
+++ pages/profile/components/profile-container/components/header/components/background/components/action-buttons/components/settings-button/index.tsx
@@ -1,10 +1,36 @@
+import { useSession } from "next-auth/react";
 import GearIcon from "public/assets/icon/gear.svg";
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
+import ActionButton from "../action-button";
+import SettingsModal from "./components/settings-modal";
 
 const SettingsButton: React.FC = () => {
+    const { data } = useSession();
+    const { first_name, last_name, bio, location, link } = data!.user;
+
+    const [settings, setSettings] = useState<{ [x: string]: string }>({
+        first_name: first_name ? first_name : "",
+        last_name: last_name ? last_name : "",
+        bio: bio ? bio : "",
+        location: location ? location : "",
+        link: link ? link : "",
+    });
+
+    const [showSettings, setShowSettings] = useState(false);
+
+    const openSettings = () => {
+        setShowSettings(true);
+    };
+
     return (
         <Fragment>
-            <GearIcon className="fill-[rgb(200,200,200)] h-[20px]" />
+            <SettingsModal
+                show={showSettings}
+                setShow={setShowSettings}
+                settings={settings}
+                setSettings={setSettings}
+            />
+            <ActionButton action={openSettings} icon={GearIcon} />
         </Fragment>
     );
 };
