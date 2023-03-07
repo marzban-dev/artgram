a30dab97c3cd70b1b2bc1f99db495a28aab7d197
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-08 01:02:37 +0330

    Update use-explore hook

--- hooks/use-explore.ts
+++ hooks/use-explore.ts
@@ -2,28 +2,10 @@ import { useInfiniteQuery } from "@tanstack/react-query";
 import { getArts } from "apis/arts.api";
 
 export const useExploreQuery = () => {
-    const pageParamDefaults = { page: 1, limit: 15 };
+    const pageParamDefaults = { limit: 15 };
 
-    return useInfiniteQuery(
-        ["explore"],
-        ({ pageParam = pageParamDefaults }) => {
-            const exploreCache = localStorage.getItem("explore-cache");
-
-            if (pageParam.page === 1 && exploreCache) {
-                return JSON.parse(exploreCache);
-            }
-
-            return getArts({ pageParam: { limit: pageParam.limit } });
-        },
-        {
-            cacheTime: Infinity,
-            staleTime: Infinity,
-            onSuccess: (data) => {
-                if (data.pages.length === 1) localStorage.setItem("explore-cache", JSON.stringify(data.pages[0]));
-            },
-            getNextPageParam: (_lastPage, pages) => {
-                return { ...pageParamDefaults, page: pages.length + 1 };
-            },
-        }
-    );
+    return useInfiniteQuery(["explore"], ({ pageParam = pageParamDefaults }) => getArts({ pageParam }), {
+        cacheTime: Infinity,
+        staleTime: Infinity,
+    });
 };
