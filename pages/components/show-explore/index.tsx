fa3fa002623963a001917eeafdd366b737fd4097
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-08 01:03:17 +0330

    Update show-explore component

--- pages/components/show-explore/index.tsx
+++ pages/components/show-explore/index.tsx
@@ -3,12 +3,17 @@ import InfiniteArts from "components/infinite-arts";
 import Spinner from "components/spinner";
 import { useExploreQuery } from "hooks/use-explore";
 import { useSearchQuery } from "hooks/use-search";
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { RootState } from "store";
 import flatInfiniteQueryData from "utils/flat-infinite-query-data";
 
 const ShowExplore: React.FC = () => {
-    const { data: arts, fetchNextPage: fetchNextExplorePage } = useExploreQuery();
+    const { data: arts, fetchNextPage: fetchNextExplorePage, refetch } = useExploreQuery();
+
+    useEffect(() => {
+        setTimeout(() => refetch(), 500);
+    }, []);
 
     const { data: searchResult } = useSearchQuery();
     const isSearching = useSelector((state: RootState) => state.explore.isSearching);
