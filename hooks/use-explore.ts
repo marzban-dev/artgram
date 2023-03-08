703267b4beda7cd99588089faa3126c0b696e1fd
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-08 21:22:29 +0330

    Update use-explore hook

--- hooks/use-explore.ts
+++ hooks/use-explore.ts
@@ -1,11 +1,16 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { getArts } from "apis/arts.api";
+import { useSelector } from "react-redux";
+import { RootState } from "store";
 
 export const useExploreQuery = () => {
+    const allowFetchNext = useSelector((state: RootState) => state.explore.allowExploreLoadMore);
+
     const pageParamDefaults = { limit: 15 };
 
     return useInfiniteQuery(["explore"], ({ pageParam = pageParamDefaults }) => getArts({ pageParam }), {
         cacheTime: Infinity,
-        staleTime: Infinity,
+        staleTime: !allowFetchNext ? 0 : Infinity,
+        getNextPageParam: () => pageParamDefaults,
     });
 };
