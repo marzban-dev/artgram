905e10536ab4379f489f86944223d9bcc1b1ac05
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-10 01:29:31 +0330

    Update art-post component

--- pages/art/components/art-post/index.tsx
+++ pages/art/components/art-post/index.tsx
@@ -47,13 +47,13 @@ const ArtPost: React.FC<IArtPostProps> = ({
     }, [imageColors, id]);
 
     return (
-        <section className="w-full flex justify-center items-center min-[520px]:mt-[30px] snap-none last:mb-[30px]">
+        <section className="flex w-full snap-none items-center justify-center last:mb-[30px] min-[520px]:mt-[30px]">
             <div
-                className="w-full min-[520px]:max-w-[450px] min-[800px]:max-w-[550px] overflow-hidden py-4 bg-black min-[520px]:bg-[rgb(20,20,20)] min-[520px]:rounded-2xl"
+                className="w-full overflow-hidden bg-black py-4 min-[520px]:max-w-[450px] min-[520px]:rounded-2xl min-[520px]:bg-[rgb(20,20,20)] min-[800px]:max-w-[550px]"
                 id={`art-container-${id}`}
                 ref={artContainer}
             >
-                <div className="w-full flex justify-between items-center px-5">
+                <div className="flex w-full items-center justify-between px-5">
                     <Header title={title} artist={artist} year={date} />
 
                     <FollowButton
@@ -64,7 +64,7 @@ const ArtPost: React.FC<IArtPostProps> = ({
                         colorClass="art"
                     />
                 </div>
-                <div className="w-full mt-4 relative">
+                <div className="relative mt-4 w-full">
                     <ArtPicture
                         id={id}
                         title={title}
@@ -82,15 +82,15 @@ const ArtPost: React.FC<IArtPostProps> = ({
                         technique={technique}
                     />
                 </div>
-                <div className="flex justify-between items-center px-5 pt-4">
-                    <div className="flex justify-start items-center gap-3">
-                        <div className="flex justify-center items-center">
+                <div className="flex items-center justify-between px-5 pt-4">
+                    <div className="flex items-center justify-start gap-3">
+                        <div className="flex items-center justify-center">
                             <Like id={id} initial={user_like} />
                         </div>
                         <LikesCount id={id} initial={likes_count} />
                     </div>
-                    <div className="flex justify-center items-center gap-4">
-                        <DownloadButton name={title + ".jpg"} href={image.url} />
+                    <div className="flex items-center justify-center gap-4">
+                        <DownloadButton name={title + ".jpg"} id={id} />
                         <Bookmark id={id} initial={user_repost} />
                     </div>
                 </div>
