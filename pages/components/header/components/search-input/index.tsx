15fdc11eb6d7075471ee179a9e2a29ab61ff813e
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-10 01:30:51 +0330

    Update search-input component

--- pages/components/header/components/search-input/index.tsx
+++ pages/components/header/components/search-input/index.tsx
@@ -1,12 +1,18 @@
+import Spinner from "components/spinner";
 import React, { Fragment } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "store";
 import FilterButton from "./components/filter-button";
 import Input from "./components/input";
 
 const SearchInput: React.FC = () => {
+    const isSearching = useSelector((state: RootState) => state.explore.isSearching);
+
     return (
         <Fragment>
-            <div className="w-[330px] min-[600px]:w-[450px] h-[55px] bg-[rgb(25,25,25)] border-2 border-[rgb(40,40,40)] rounded-[30px] px-5 z-20 flex justify-start items-center focus-within:border-[rgb(60,60,60)] transition-colors">
+            <div className="z-20 flex h-[55px] w-[330px] items-center justify-start rounded-[30px] border-2 border-[rgb(40,40,40)] bg-[rgb(25,25,25)] px-5 transition-colors focus-within:border-[rgb(60,60,60)] min-[600px]:w-[450px]">
                 <Input />
+                {isSearching && <Spinner style={{ padding: "0 15px" }} size={22} />}
                 <FilterButton />
             </div>
         </Fragment>
