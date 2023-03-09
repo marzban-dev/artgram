42f8db605fac75a9fae7c4e831dcdbc0cd3aca38
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-10 01:31:02 +0330

    Update show-explore component

--- pages/components/show-explore/index.tsx
+++ pages/components/show-explore/index.tsx
@@ -4,7 +4,7 @@ import InfiniteArts from "components/infinite-arts";
 import Spinner from "components/spinner";
 import { useExploreQuery } from "hooks/use-explore";
 import { useSearchQuery } from "hooks/use-search";
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "store";
 import { setAllowExploreLoadMore } from "store/slice/explore.slice";
@@ -35,18 +35,24 @@ const ShowExplore: React.FC = () => {
     }, [allowFetchNext]);
 
     const { data: searchResult } = useSearchQuery();
-    const isSearching = useSelector((state: RootState) => state.explore.isSearching);
 
-    if (arts && !searchResult && !isSearching) {
+    if (arts && !searchResult) {
         return (
-            <InfiniteArts
-                arts={flatInfiniteQueryData<IArt>(arts!)}
-                callback={fetchNextPage}
-                hasNextPage={allowFetchNext}
-            />
+            <Fragment>
+                {!allowFetchNext && (
+                    <div className="flex w-full items-center justify-center pt-2 pb-6">
+                        <Spinner size={40} />
+                    </div>
+                )}
+                <InfiniteArts
+                    arts={flatInfiniteQueryData<IArt>(arts!)}
+                    callback={fetchNextPage}
+                    hasNextPage={allowFetchNext}
+                />
+            </Fragment>
         );
     } else {
-        if (!arts) return <Spinner size={30} />;
+        if (!arts) return <Spinner size={40} />;
         else return null;
     }
 };
