9672ee54d7015ed1717b95eb43caea270c1154cb
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-13 01:24:39 +0330

    Update settings-button component

--- pages/profile/components/profile-container/components/header/components/background/components/action-buttons/components/settings-button/index.tsx
+++ pages/profile/components/profile-container/components/header/components/background/components/action-buttons/components/settings-button/index.tsx
@@ -1,36 +1,48 @@
+import { useUpdateSettings } from "hooks/use-update-settings";
+import { useUserQuery } from "hooks/use-user";
 import { useSession } from "next-auth/react";
 import GearIcon from "public/assets/icon/gear.svg";
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useCallback, useMemo, useState } from "react";
 import ActionButton from "../action-button";
 import SettingsModal from "./components/settings-modal";
 
 const SettingsButton: React.FC = () => {
-    const { data } = useSession();
-    const { first_name, last_name, bio, location, link } = data!.user;
+    const { data: authData } = useSession();
+    const { data: userData } = useUserQuery(authData!.user.username);
 
-    const [settings, setSettings] = useState<{ [x: string]: string }>({
-        first_name: first_name ? first_name : "",
-        last_name: last_name ? last_name : "",
-        bio: bio ? bio : "",
-        location: location ? location : "",
-        link: link ? link : "",
-    });
-
+    const { mutateAsync } = useUpdateSettings();
     const [showSettings, setShowSettings] = useState(false);
 
-    const openSettings = () => {
-        setShowSettings(true);
-    };
+    const userUpdatableData = useMemo(() => {
+        return {
+            first_name: userData?.first_name ?? "",
+            last_name: userData?.last_name ?? "",
+            bio: userData?.bio ?? "",
+            location: userData?.location ?? "",
+            link: userData?.link ?? "",
+        };
+    }, [userData]);
+
+    const sendUpdateRequest = useCallback(
+        async (newSettings: object) => {
+            if (JSON.stringify(userUpdatableData) !== JSON.stringify(newSettings)) {
+                console.log("Updating ...");
+                await mutateAsync(newSettings);
+                console.log("Updated");
+            }
+        },
+        [userUpdatableData]
+    );
 
     return (
         <Fragment>
             <SettingsModal
                 show={showSettings}
                 setShow={setShowSettings}
-                settings={settings}
-                setSettings={setSettings}
+                userData={userUpdatableData}
+                sendUpdateRequest={sendUpdateRequest}
             />
-            <ActionButton action={openSettings} icon={GearIcon} />
+            <ActionButton action={() => setShowSettings(true)} icon={GearIcon} />
         </Fragment>
     );
 };
