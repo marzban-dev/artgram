db675ec8ee9c7fa3c1233df5f1758677c2316b8d
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-13 01:23:11 +0330

    Update header component

--- pages/art/components/art-post/components/header/index.tsx
+++ pages/art/components/art-post/components/header/index.tsx
@@ -1,4 +1,5 @@
 import Avatar from "components/avatar";
+import TextOverflowAnimation from "components/text-overflow-animation";
 import Link from "next/link";
 import { IHeaderProps } from "./header.types";
 
@@ -6,20 +7,22 @@ const Header: React.FC<IHeaderProps> = ({ title, artist, year }) => {
     const route = `/profile/artist/${artist.id}`;
 
     return (
-        <div className="flex justify-start items-center gap-3 min-[800px]:gap-4 w-[calc(100%_-_100px)]">
+        <div className="flex w-[calc(100%_-_100px)] items-center justify-start gap-2 min-[800px]:gap-3">
             <Link href={route}>
                 <Avatar
-                    className="w-[45px] min-[800px]:w-[60px] h-[45px] min-[800px]:h-[60px]"
+                    className="h-[45px] w-[45px] min-[800px]:h-[60px] min-[800px]:w-[60px]"
                     picture={artist.image}
                     title={title}
                 />
             </Link>
-            <div className="flex justify-start items-start flex-col max-w-[calc(100%_-_100px)]">
-                <h2 className="text-white text-[16px] min-[800px]:text-[20px] overflow-hidden whitespace-nowrap text-ellipsis w-full">
-                    {title}
+            <div className="flex max-w-[calc(100%_-_100px)] flex-col items-start justify-start">
+                <h2 className="w-full overflow-hidden text-ellipsis text-[16px] text-white min-[800px]:text-[20px]">
+                    <TextOverflowAnimation className="px-2">{title}</TextOverflowAnimation>
                 </h2>
-                <div className="text-[rgb(150,150,150)] text-[12px] min-[800px]:text-[15px] overflow-hidden whitespace-nowrap text-ellipsis w-full">
-                    <Link href={route}>{artist.name}</Link> - <span>{year}</span>
+                <div className="w-full overflow-hidden text-ellipsis whitespace-nowrap text-[12px] text-[rgb(150,150,150)] min-[800px]:text-[15px]">
+                    <TextOverflowAnimation className="px-2">
+                        <Link href={route}>{artist.name}</Link>- <span>{year}</span>
+                    </TextOverflowAnimation>
                 </div>
             </div>
         </div>
