1ea8d9a4f91059b034aa349ba7676748b2648195
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-13 01:22:02 +0330

    Update user component

--- components/user/index.tsx
+++ components/user/index.tsx
@@ -1,5 +1,6 @@
 import Avatar from "components/avatar";
 import FollowButton from "components/follow-button";
+import TextOverflowAnimation from "components/text-overflow-animation";
 import Link from "next/link";
 import { IUserProps } from "./user.types";
 
@@ -7,13 +8,24 @@ const User: React.FC<IUserProps> = ({ id, avatar, title, type, underTitle, isFol
     const href = `/profile/${type}/${id}`;
 
     return (
-        <div className="w-full flex justify-start items-center gap-4 h-[60px]">
+        <div className="flex h-[60px] w-full items-center justify-start gap-2">
             <Link href={href}>
                 <Avatar picture={avatar} title={title} />
             </Link>
-            <Link href={href} className="h-full w-[calc(100%-190px)] flex flex-col justify-center items-start text-white">
-                <span className="text-[18px]">{title}</span>
-                <span className="text-[rgb(150,150,150)] text-[16px]">{underTitle}</span>
+            <Link
+                href={href}
+                className="flex h-full w-[calc(100%-180px)] flex-col items-start justify-center text-white"
+            >
+                <div className="w-[90%]">
+                    <TextOverflowAnimation className="px-2 text-[18px]">
+                        {title}
+                    </TextOverflowAnimation>
+                </div>
+                <div className="w-[90%]">    
+                    <TextOverflowAnimation className="px-2 text-[16px] text-[rgb(150,150,150)]">
+                        {underTitle}
+                    </TextOverflowAnimation>
+                </div>
             </Link>
             <FollowButton id={id} type={type} width={100} colorClass="profile" initial={isFollowing} />
         </div>
