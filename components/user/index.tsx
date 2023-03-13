ddeb0604b7cee085ffba992cc28ea0ddef929729
Mansour <84951627+marzban-dev@users.noreply.github.com>
2023-03-13 23:05:24 +0330

    Update user component

--- components/user/index.tsx
+++ components/user/index.tsx
@@ -1,6 +1,5 @@
 import Avatar from "components/avatar";
 import FollowButton from "components/follow-button";
-import TextOverflowAnimation from "components/text-overflow-animation";
 import Link from "next/link";
 import { IUserProps } from "./user.types";
 
@@ -16,16 +15,8 @@ const User: React.FC<IUserProps> = ({ id, avatar, title, type, underTitle, isFol
                 href={href}
                 className="flex h-full w-[calc(100%-180px)] flex-col items-start justify-center text-white"
             >
-                <div className="w-[90%]">
-                    <TextOverflowAnimation className="px-2 text-[18px]">
-                        {title}
-                    </TextOverflowAnimation>
-                </div>
-                <div className="w-[90%]">    
-                    <TextOverflowAnimation className="px-2 text-[16px] text-[rgb(150,150,150)]">
-                        {underTitle}
-                    </TextOverflowAnimation>
-                </div>
+                <div className="w-[90%] text-[18px]">{title}</div>
+                <div className="w-[90%] text-[16px] text-[rgb(150,150,150)]">{underTitle}</div>
             </Link>
             <FollowButton id={id} type={type} width={100} colorClass="profile" initial={isFollowing} />
         </div>
